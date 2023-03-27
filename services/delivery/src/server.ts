d7f445ac3dd71e854089b60491eff4d747f23c8f
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2023-03-27 11:42:00 +0200

    feat: init delivery microservice (typescript, prisma, grpc)
    * add prisma (schema, singleton, seed)
    * add grpc implementation
    * add logger
    * add file structure with proper tsconfig
    * readme file model

---
+++ services/delivery/src/server.ts
@@ -0,0 +1,35 @@
+import "dotenv/config";
+
+import { loadSync } from "@grpc/proto-loader";
+import { loadPackageDefinition, Server } from "@grpc/grpc-js";
+
+import { log, utils } from "@delivery/lib/log";
+import { options } from "@delivery/resources/protoloader-options";
+import { serverInsecure } from "@delivery/resources/grpc-credentials";
+
+const PORT = process.env.PORT || 50008;
+const ADDRESS = `0.0.0.0:${PORT}`;
+const PROTO_PATH = __dirname + "/../../proto/delivery.proto";
+
+const packageDefinition = loadSync(PROTO_PATH, options);
+const { delivery } = loadPackageDefinition(packageDefinition) as any; // todo: fix any
+const server = new Server();
+
+server.addService(delivery.DeliveryService.service, {
+  // handlers
+  // todo: add handlers
+});
+
+server.addService(delivery.DeliveryPersonService.service, {
+  // handlers
+  // todo: add handlers
+});
+
+server.bindAsync(ADDRESS, serverInsecure, () => {
+  const message = `---- ${utils.green("good")}${utils.yellow(
+    "food"
+  )} Delivery Service ----\nstarted on: ${utils.bold(ADDRESS)} ${utils.green(
+    "✓"
+  )}\n`;
+  log.debug(message);
+});
