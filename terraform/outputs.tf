02c53718a415ae90e9370080addfa0bedc874b8f
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2023-04-10 11:29:33 +0200

    feat(tf): add terraform files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,8 @@
 output "main-rg-name" {
-  value = data.azurerm_resource_group.rg-fleroux.name
+  value = data.azurerm_resource_group.rg-goodfood.name
 }
 output "main-rg-id" {
-  value = data.azurerm_resource_group.rg-fleroux.id
+  value = data.azurerm_resource_group.rg-goodfood.id
 }
 
 output "client_certificate" {
