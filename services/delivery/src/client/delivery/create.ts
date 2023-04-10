038b1f7f5a493dc5eef5dbd0304276e183105874
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2023-04-10 14:34:43 +0200

    ci: add tests (but test script is disabeld)

--- services/delivery/src/client/delivery/create.ts
+++ services/delivery/src/client/delivery/create.ts
@@ -5,6 +5,7 @@ import { loadPackageDefinition } from "@grpc/grpc-js";
 import { options } from "@delivery/resources/protoloader-options";
 import { insecure } from "@delivery/resources/grpc-credentials";
 import { log } from "@delivery/lib/log";
+import { Delivery } from "@delivery/types/delivery";
 
 const PORT = process.env.PORT || 50008;
 const ADDRESS = `localhost:${PORT}`;
@@ -13,21 +14,28 @@ const PROTO_PATH = __dirname + "/../../../../proto/delivery.proto";
 const packageDefinition = loadSync(PROTO_PATH, options);
 const { delivery } = loadPackageDefinition(packageDefinition) as any;
 
-function main() {
+const DEFAULT_DATA = {
+  eta: "2022-01-01T00:00:00.000Z",
+  address: "10 Rue de la République, 75003 Paris, France",
+  status: "PENDING",
+  delivery_person_id: "random_id",
+  restaurant_id: "restaurant_id:1",
+  user_id: "user_id:1",
+};
+
+export async function main(data = DEFAULT_DATA) {
   const client = new delivery.DeliveryService(ADDRESS, insecure);
 
-  const data = {
-    eta: "2022-01-01T00:00:00.000Z",
-    address: "10 Rue de la République, 75003 Paris, France",
-    status: "PENDING",
-    delivery_person_id: "random_id",
-    restaurant_id: "restaurant_id:1",
-    user_id: "user_id:1",
-  };
-  client.CreateDelivery(data, (err: any, response: any) => {
-    if (err) log.debug(err);
-    log.debug(response);
-  });
+  const response: Delivery | null = await client.CreateDelivery(
+    data,
+    async (err: any, response: any) => {
+      if (err) log.debug(err);
+      log.debug(response);
+      return Promise.resolve(response);
+    }
+  );
+
+  return response;
 }
 
 main();
--- services/delivery/vitest.config.ts
+++ services/delivery/vitest.config.ts
@@ -4,4 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   base: "/src/tests",
+  test: {
+    setupFiles: ["./src/tests/setup.ts"],
+  },
 });
