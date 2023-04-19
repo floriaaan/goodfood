dcd9d7b253af4aa375d5095dd45a5192a274bf50
anatole <anatole.godard@icloud.com>
2023-04-19 15:23:32 +0200

    feat(terraform): update general terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,6 +4,40 @@ output "main-rg-name" {
 output "main-rg-id" {
   value = data.azurerm_resource_group.rg-goodfood.id
 }
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.aks-goodfood.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "client_key" {
+  value     = azurerm_kubernetes_cluster.aks-goodfood.kube_config[0].client_key
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = azurerm_kubernetes_cluster.aks-goodfood.kube_config[0].cluster_ca_certificate
+  sensitive = true
+}
+
+output "cluster_password" {
+  value     = azurerm_kubernetes_cluster.aks-goodfood.kube_config[0].password
+  sensitive = true
+}
+
+output "cluster_username" {
+  value     = azurerm_kubernetes_cluster.aks-goodfood.kube_config[0].username
+  sensitive = true
+}
+
+output "host" {
+  value     = azurerm_kubernetes_cluster.aks-goodfood.kube_config[0].host
+  sensitive = true
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks-goodfood.kube_config_raw
+  sensitive = true
+}
 
 output "resource_group_name" {
   value = data.azurerm_resource_group.rg-goodfood.name
