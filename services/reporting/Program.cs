6a1225e121faae7ae789c15a69d62ff44a74927f
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2023-04-19 11:41:33 +0200

    ref(upgrade): migrate from dotnet6 to dotnet8-preview

--- services/reporting/Program.cs
+++ services/reporting/Program.cs
@@ -1,10 +1,20 @@
 using reporting.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
 
 var app = builder.Build();
+
+IWebHostEnvironment env = app.Environment;
+
+if (env.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
