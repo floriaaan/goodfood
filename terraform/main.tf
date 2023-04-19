0ac1efa923abe33ba18499e765dff5462524c3b1
anatole <anatole.godard@icloud.com>
2023-04-19 15:23:32 +0200

    feat(terraform): update general terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,58 +69,27 @@ resource "azurerm_postgresql_server" "pgsql-goodfood" {
   location            = data.azurerm_resource_group.rg-goodfood.location
   resource_group_name = data.azurerm_resource_group.rg-goodfood.name
 
-  sku_name = "B_Gen5_2"
 
-  storage_mb                   = 5120
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
-  auto_grow_enabled            = true
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D2_v2"
+    node_count = var.agent_count
+  }
+  linux_profile {
+    admin_username = "ubuntu"
 
-  administrator_login              = var.db_username
-  administrator_login_password     = var.db_password
-  version                          = "9.5"
-  ssl_enforcement_enabled          = false
-  ssl_minimal_tls_version_enforced = "TLSEnforcementDisabled"
-}
-
-resource "azurerm_postgresql_firewall_rule" "pg-fw-goodfood" {
-  name                = "firewall-${var.project_name}${var.environnment_suffix}"
-  resource_group_name = data.azurerm_resource_group.rg-goodfood.name
-  server_name         = azurerm_postgresql_server.pgsql-goodfood.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
-}
-
-resource "azurerm_postgresql_database" "db-goodfood" {
-  name                = "postgres-${var.project_name}${var.environnment_suffix}"
-  resource_group_name = data.azurerm_resource_group.rg-goodfood.name
-  server_name         = azurerm_postgresql_server.pgsql-goodfood.name
-  charset             = "UTF8"
-  collation           = "English_United States.1252"
-}
-
-resource "azurerm_container_group" "container_group" {
-  name                = "container-${var.project_name}${var.environnment_suffix}"
-  location            = data.azurerm_resource_group.rg-goodfood.location
-  resource_group_name = data.azurerm_resource_group.rg-goodfood.name
-  ip_address_type     = "Public"
-  dns_name_label      = "container-${var.project_name}${var.environnment_suffix}"
-  os_type             = "Linux"
-
-  container {
-    name   = "${var.project_name}${var.environnment_suffix}"
-    image  = "floriaaan/goodfood-delivery:latest"
-    cpu    = "0.5"
-    memory = "1.5"
-
-    ports {
-      port     = 50008
-      protocol = "TCP"
+    ssh_key {
+      key_data = file(var.ssh_public_key)
     }
+  }
 
-    environment_variables = {
-      "DATABASE_URL" = "postgres://${var.db_username}@${azurerm_postgresql_server.pgsql-goodfood.name}:${var.db_password}@${azurerm_postgresql_server.pgsql-goodfood.fqdn}:5432/${azurerm_postgresql_database.db-goodfood.name}"
-      "PORT"         = 50008
-    }
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+
+  service_principal {
+    client_id     = var.aks_service_principal_app_id
+    client_secret = var.aks_service_principal_client_secret
   }
 }
