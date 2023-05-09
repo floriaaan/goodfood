e548effc21294e35d3aa2a2a40f53124591f1891
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2023-05-09 14:37:37 +0200

    feat(grpc): add grpc config files

---
+++ services/order/src/types/order.d.ts
@@ -0,0 +1,48 @@
+import { Status } from "@prisma/client";
+
+export type UserMinimum = {
+  id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+};
+
+export type Basket = {
+  json: string; // TODO: to be implemented
+};
+
+export type Order = {
+  id: string;
+  payment_id: string;
+  delivery_id: string;
+  user: UserMinimum;
+  basket_snapshot: Basket;
+  status: Status;
+};
+
+export type GetOrderRequest = {
+  id: string;
+};
+
+export type CreateOrderRequest = {
+  payment_id: string;
+  delivery_id: string;
+  user: UserMinimum;
+  basket_snapshot: Basket;
+};
+
+export type UpdateOrderRequest = {
+  id: string;
+  payment_id: string;
+  delivery_id: string;
+  status: Status;
+};
+
+export type DeleteOrderRequest = {
+  id: string;
+};
+
+export type DeleteOrderResponse = {
+  id: string;
+};
