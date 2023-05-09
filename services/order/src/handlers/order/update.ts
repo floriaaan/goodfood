8cd3a572edc02a93cf9839e12ba8fad996f1f6c8
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2023-05-09 14:38:00 +0200

    feat(handlers): add handlers

---
+++ services/order/src/handlers/order/update.ts
@@ -0,0 +1,31 @@
+import { prisma } from "@order/lib/prisma";
+import { log } from "@order/lib/log";
+import { UpdateOrderRequest, Order } from "@order/types/order";
+import { Data } from "@order/types";
+
+export const UpdateOrder = async (
+  { request }: Data<UpdateOrderRequest>,
+  callback: (err: any, response: Order | null) => void
+) => {
+  log.debug("request received at UpdateOrder handler\n", request);
+  try {
+    const { delivery_id, payment_id, id, status } = request;
+
+    // todo: calculate total from basket_snapshot
+    const total = 0; // basket_snapshot.reduce;
+
+    const order = await prisma.order.update({
+      where: { id },
+      data: {
+        delivery_id,
+        payment_id,
+        status,
+      },
+      include: { basket_snapshot: true, user: true },
+    });
+    callback(null, order);
+  } catch (error) {
+    log.error(error);
+    callback(error, null);
+  }
+};
