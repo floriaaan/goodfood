4edfda37e54595319c113cf2913f20b4e4ddff41
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2023-05-10 15:02:41 +0200

    feat(methods): add methods to proto, handlers, reflection

--- services/order/src/handlers/order/index.ts
+++ services/order/src/handlers/order/index.ts
@@ -1,15 +1,25 @@
 import { UntypedServiceImplementation } from "@grpc/grpc-js";
 
-import { CreateOrder } from "./create";
-import { GetOrder } from "./get";
-import { UpdateOrder } from "./update";
-import { DeleteOrder } from "./delete";
+import { CreateOrder } from "@order/handlers/order/create";
+import { GetOrder } from "@order/handlers/order/get";
+import { UpdateOrder } from "@order/handlers/order/update";
+import { DeleteOrder } from "@order/handlers/order/delete";
+
+import { GetOrdersByUser } from "@order/handlers/order/get-user";
+import { GetOrderByDelivery } from "@order/handlers/order/get-delivery";
+import { GetOrderByPayment } from "@order/handlers/order/get-payment";
+import { GetOrdersByStatus } from "@order/handlers/order/get-status";
 
 const orderHandlers: UntypedServiceImplementation = {
   CreateOrder,
   GetOrder,
   UpdateOrder,
   DeleteOrder,
+
+  GetOrdersByUser,
+  GetOrderByDelivery,
+  GetOrderByPayment,
+  GetOrdersByStatus,
 };
 
 export default orderHandlers;
