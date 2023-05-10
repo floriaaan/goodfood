fabd2612ccce7e05818d50a65b9c37877ece7abc
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2023-05-10 09:00:31 +0200

    feat(data-structure): add toGrpc, parseStruct & toStruct methods

--- services/order/prisma/seed.ts
+++ services/order/prisma/seed.ts
@@ -1,17 +1,97 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient, Prisma, Status } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-
+const orders: Prisma.OrderCreateInput[] = [
+  {
+    id: "order_id:1",
+    payment_id: "payment_id:1",
+    delivery_id: "delivery_id:1",
+    user: {
+      connectOrCreate: {
+        where: { id: "user_id:1" },
+        create: {
+          id: "user_id:1",
+          first_name: "John",
+          last_name: "Doe",
+          email: "john@doe.com",
+          phone: "0612345678",
+        },
+      },
+    },
+    basket_snapshot: {
+      create: {
+        json: {},
+        string: JSON.stringify({}),
+        total: 0,
+      },
+    },
+    status: Status.PENDING,
+  },
+  {
+    id: "order_id:2",
+    payment_id: "payment_id:2",
+    delivery_id: "delivery_id:2",
+    user: {
+      connectOrCreate: {
+        where: { id: "user_id:1" },
+        create: {
+          id: "user_id:1",
+          first_name: "John",
+          last_name: "Doe",
+          email: "john@doe.com",
+          phone: "0612345678",
+        },
+      },
+    },
+    basket_snapshot: {
+      create: {
+        json: {},
+        string: JSON.stringify({}),
+        total: 0,
+      },
+    },
+    status: Status.FULFILLED,
+  },
+  {
+    id: "order_id:3",
+    payment_id: "payment_id:3",
+    delivery_id: "delivery_id:3",
+    user: {
+      connectOrCreate: {
+        where: { id: "user_id:2" },
+        create: {
+          id: "user_id:2",
+          first_name: "Foonie",
+          last_name: "Garbage",
+          email: "foo@gar.com",
+          phone: "0612345678",
+        },
+      },
+    },
+    basket_snapshot: {
+      create: {
+        json: {},
+        string: JSON.stringify({}),
+        total: 0,
+      },
+    },
+    status: Status.PENDING,
+  },
+];
 
 async function main() {
+  console.log(`Delete existing data ...`);
+  await prisma.order.deleteMany();
+  await prisma.userMinimum.deleteMany();
+  await prisma.basket.deleteMany();
+  console.log(`Existing data deleted.`);
+
   console.log(`Start seeding ...`);
-  // for (const d of deliveries) {
-  //   const delivery = await prisma.delivery.create({
-  //     data: d,
-  //   });
-  //   console.log(`Created delivery with id: ${delivery.id}`);
-  // }
+  for (const o of orders) {
+    const order = await prisma.order.create({ data: o });
+    console.log(`Created order with id: ${order.id}`);
+  }
   console.log(`Seeding finished.`);
 }
 
