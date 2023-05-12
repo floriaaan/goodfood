0d4b9d33cd0f4f63f0f02811297883831625d1bf
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2023-05-12 15:24:06 +0200

    test: remove tests

--- services/delivery/vitest.config.ts
+++ services/delivery/vitest.config.ts
@@ -4,7 +4,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   base: "/src/tests",
-  test: {
-    setupFiles: ["./src/tests/setup.ts"],
-  },
 });
