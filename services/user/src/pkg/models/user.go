be7e7327c9944a130e926a0952ffde1d734d1b38
anatole <anatole.godard@icloud.com>
2023-07-03 11:56:52 +0200

    fix(models): update models to fix the migration

--- services/user/src/pkg/models/user.go
+++ services/user/src/pkg/models/user.go
@@ -1,12 +1,14 @@
 package models
 
 type User struct {
-	Id          string      `json:"id" gorm:"primaryKey"`
-	Email       string      `json:"email" gorm:"type:varchar(255);not null"`
-	Password    string      `json:"password" gorm:"type:varchar(255);not null"`
-	Phone       string      `json:"phone" gorm:"type:varchar(10)"`
-	LastName    string      `json:"last_name" gorm:"type:varchar(100);not null"`
-	FirstName   string      `json:"first_name" gorm:"type:varchar(100);not null"`
-	MainAddress MainAddress `json:"main_address" gorm:"foreignKey:main_address_id"`
-	Role        Role        `json:"role" gorm:"foreignKey:RoleId"`
+	Id            string `json:"id" gorm:"primaryKey"`
+	Email         string `json:"email" gorm:"type:varchar(255);not null"`
+	Password      string `json:"password" gorm:"type:varchar(255);not null"`
+	Phone         string `json:"phone" gorm:"type:varchar(10)"`
+	LastName      string `json:"last_name" gorm:"type:varchar(100);not null"`
+	FirstName     string `json:"first_name" gorm:"type:varchar(100);not null"`
+	MainAddressId string
+	MainAddress   MainAddress `json:"main_address" gorm:"foreignKey:MainAddressId;references:Id"`
+	RoleId        string
+	Role          Role `json:"role" gorm:"foreignKey:RoleId"`
 }
