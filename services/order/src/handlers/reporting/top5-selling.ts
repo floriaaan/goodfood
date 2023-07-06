c11076128470518931aa147e241b57496d467dca
Florian LEROUX <florian.leroux3@laposte.net>
2023-07-06 11:12:13 +0200

    ref(date): split function and write tests

--- services/order/src/handlers/reporting/top5-selling.ts
+++ services/order/src/handlers/reporting/top5-selling.ts
@@ -6,9 +6,7 @@ import {
   ProductCount,
 } from "@order/types/reporting";
 import { Data } from "@order/types";
-
-const HOUR_IN_MS = 60 * 60 * 1000;
-const DAY_IN_MS = 24 * HOUR_IN_MS;
+import { fromInterval } from "@order/lib/date";
 
 export const GetTop5SellingProducts = async (
   { request }: Data<GetTop5SellingProductsRequest>,
@@ -16,20 +14,7 @@ export const GetTop5SellingProducts = async (
 ) => {
   try {
     const { date, restaurant_id, interval } = request;
-
-    const end = date
-      ? new Date(date + " 23:59:59")
-      : new Date(new Date().setHours(23, 59, 59, 999));
-
-    const start = // end - interval
-      interval === "1w"
-        ? new Date(end.getTime() - 7 * DAY_IN_MS)
-        : interval === "1m"
-        ? new Date(end.getTime() - 30 * DAY_IN_MS)
-        : interval === "1y"
-        ? new Date(end.getTime() - 365 * DAY_IN_MS)
-        : // default to 1w
-          new Date(end.getTime() - 7 * DAY_IN_MS);
+    const { start, end } = fromInterval(interval, date);
 
     const orders = await prisma.order.findMany({
       where: { restaurant_id, created_at: { gte: start, lte: end } },
@@ -52,7 +37,6 @@ export const GetTop5SellingProducts = async (
       }, [] as ProductCount[])
       .sort((a, b) => b.count - a.count)
       .slice(0, 5);
-
 
     callback(null, {
       products_count,
