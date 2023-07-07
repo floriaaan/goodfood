9977ccae0a47a795aa60a618f7525c7dba18012d
Florian LEROUX <florian.leroux3@laposte.net>
2023-07-07 15:33:19 +0200

    feat(ingredient): add ingredient handler

--- services/stock/src/types/stock.d.ts
+++ services/stock/src/types/stock.d.ts
@@ -1,11 +1,12 @@
 export type Ingredient = {
   id: number;
   name: string;
-  description?: string;
+  description: string | null;
 };
 
 export type IngredientRestaurant = {
   id: number;
+  key: string;
 
   alert_threshold: number;
   quantity: number;
@@ -31,6 +32,22 @@ export type Supplier = {
   contact: string;
 };
 
+export type SupplyOrder = {
+  id: number;
+
+  quantity: number;
+  unit_price: number;
+
+  ingredient_restaurant_id: number;
+  ingredient_restaurant: IngredientRestaurant;
+
+  supplier_id: number;
+  supplier: Supplier;
+
+  created_at: string | Date;
+  updated_at: string | Date;
+};
+
 export type GetIngredientRequest = {
   id: number;
 };
@@ -41,11 +58,13 @@ export type GetIngredientsResponse = {
 
 export type CreateIngredientRequest = {
   name: string;
+  description?: string;
 };
 
 export type UpdateIngredientRequest = {
   id: number;
   name: string;
+  description?: string;
 };
 
 export type DeleteIngredientRequest = {
@@ -95,6 +114,14 @@ export type GetIngredientRestaurantsByRestaurantResponse = {
   ingredient_restaurants: IngredientRestaurant[];
 };
 
+export type GetIngredientRestaurantsByProductRequest = {
+  product_id: string;
+};
+
+export type GetIngredientRestaurantsByProductResponse = {
+  ingredient_restaurants: IngredientRestaurant[];
+};
+
 export type CreateIngredientRestaurantRequest = {
   alert_threshold: number;
   quantity: number;
@@ -127,5 +154,57 @@ export type DeleteIngredientRestaurantRequest = {
 };
 
 export type DeleteIngredientRestaurantResponse = {
+  success: boolean;
+};
+
+export type GetSupplyOrderRequest = {
+  id: number;
+};
+
+export type GetSupplyOrdersByRestaurantRequest = {
+  restaurant_id: string;
+};
+
+export type GetSupplyOrdersByRestaurantResponse = {
+  supply_orders: SupplyOrder[];
+};
+
+export type GetSupplyOrdersBySupplierRequest = {
+  supplier_id: number;
+};
+
+export type GetSupplyOrdersBySupplierResponse = {
+  supply_orders: SupplyOrder[];
+};
+
+export type GetSupplyOrdersByIngredientRestaurantRequest = {
+  ingredient_restaurant_id: number;
+};
+
+export type GetSupplyOrdersByIngredientRestaurantResponse = {
+  supply_orders: SupplyOrder[];
+};
+
+export type CreateSupplyOrderRequest = {
+  quantity: number;
+
+  ingredient_restaurant_id: number;
+  supplier_id: number;
+};
+
+export type UpdateSupplyOrderRequest = {
+  id: number;
+  quantity: number;
+  unit_price: number;
+
+  ingredient_restaurant_id: number;
+  supplier_id: number;
+};
+
+export type DeleteSupplyOrderRequest = {
+  id: number;
+};
+
+export type DeleteSupplyOrderResponse = {
   success: boolean;
 };
