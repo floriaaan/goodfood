d4cc04c18200d598c0371ce54d07ca65b6683550
Florian LEROUX <florian.leroux3@laposte.net>
2023-07-07 09:06:23 +0200

    feat(init): init service code and add proto/type/schema definitions

---
+++ services/stock/src/middleware/log.ts
@@ -0,0 +1,42 @@
+import { publish } from "@stock/lib/amqp";
+import { utils } from "@stock/lib/log";
+import { log } from "@stock/lib/log";
+
+type Context = {
+  call: {
+    request: Map<string, any>;
+  };
+  service: {
+    path: string;
+  };
+};
+
+const parseContext = (ctx: Context) => {
+  const {
+    call: { request },
+    service: { path },
+  } = ctx;
+  return { request, path };
+};
+
+export const msg = (
+  type: "GRPC" | "AMQP",
+  path: string | undefined,
+  requestAt: Date,
+  responseAt: Date
+) =>
+  `${utils.magenta(requestAt.toISOString())} | ${utils[
+    type === "GRPC" ? "cyan" : "red"
+  ](type)}${path ? ` | ${utils.yellow(path)}` : ""} | ${utils.green(
+    responseAt.getTime() - requestAt.getTime() + "ms"
+  )} |`;
+
+export const logGRPC = async (ctx: Context, next: () => Promise<void>) => {
+  const requestAt = new Date();
+  await next();
+  const responseAt = new Date();
+  let { request, path } = parseContext(ctx);
+  path = path?.split(".").at(-1) || "";
+  publish({ request, path }, "log");
+  log.debug(msg("GRPC", path, requestAt, responseAt), JSON.stringify(request));
+};
