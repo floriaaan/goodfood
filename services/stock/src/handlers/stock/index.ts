5cf39da8162800f2dcac822c1843c6fc3d65cef9
Florian LEROUX <florian.leroux3@laposte.net>
2023-07-07 15:35:09 +0200

    feat(handlers): add handlers

--- services/stock/src/handlers/stock/index.ts
+++ services/stock/src/handlers/stock/index.ts
@@ -1,5 +1,67 @@
 import { UntypedServiceImplementation } from "@grpc/grpc-js";
 
-const stockHandlers: UntypedServiceImplementation = {};
+import {
+  GetIngredient,
+  GetIngredients,
+  CreateIngredient,
+  UpdateIngredient,
+  DeleteIngredient,
+} from "@stock/handlers/stock/ingredient";
+
+import {
+  GetSupplier,
+  GetSuppliers,
+  CreateSupplier,
+  UpdateSupplier,
+  DeleteSupplier,
+} from "@stock/handlers/stock/supplier";
+
+import {
+  GetIngredientRestaurant,
+  GetIngredientRestaurantsByRestaurant,
+  GetIngredientRestaurantsByProduct,
+  CreateIngredientRestaurant,
+  UpdateIngredientRestaurant,
+  DeleteIngredientRestaurant,
+} from "@stock/handlers/stock/ingredient_restaurant";
+
+import {
+  GetSupplyOrder,
+  GetSupplyOrdersByIngredientRestaurant,
+  GetSupplyOrdersByRestaurant,
+  GetSupplyOrdersBySupplier,
+  CreateSupplyOrder,
+  UpdateSupplyOrder,
+  DeleteSupplyOrder,
+} from "@stock/handlers/stock/supply_order";
+
+const stockHandlers: UntypedServiceImplementation = {
+  GetIngredient,
+  GetIngredients,
+  CreateIngredient,
+  UpdateIngredient,
+  DeleteIngredient,
+
+  GetSupplier,
+  GetSuppliers,
+  CreateSupplier,
+  UpdateSupplier,
+  DeleteSupplier,
+
+  GetIngredientRestaurant,
+  GetIngredientRestaurantsByRestaurant,
+  GetIngredientRestaurantsByProduct,
+  CreateIngredientRestaurant,
+  UpdateIngredientRestaurant,
+  DeleteIngredientRestaurant,
+
+  GetSupplyOrder,
+  GetSupplyOrdersByIngredientRestaurant,
+  GetSupplyOrdersByRestaurant,
+  GetSupplyOrdersBySupplier,
+  CreateSupplyOrder,
+  UpdateSupplyOrder,
+  DeleteSupplyOrder,
+};
 
 export default stockHandlers;
