5ed0f26eb41d8e7ceac7bb668c8b0907ee392f21
anatole <anatole.godard@icloud.com>
2023-07-19 00:39:57 +0200

    feat(user controller): create userController and user grpc client service

--- services/gateway/src/server.ts
+++ services/gateway/src/server.ts
@@ -1,4 +1,5 @@
 import {Request, Response} from "express";
+import {userController} from "@order/controller/user/userController";
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 50000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Node.js API Gateway is up and running!');
 });
+
+userController(app);
 
 app.listen(PORT, () => {
     console.log(`API Gateway is running on port ${PORT}`);
