eb4b7b9c73b38c5e1f82d430efa8fb1f7478485e
Pierre Lebigre <pierre.lebigre@outlook.fr>
2023-07-21 15:27:42 +0200

    Add functionnalities

--- services/promotions/src/handler/list-all.ts
+++ services/promotions/src/handler/list-all.ts
@@ -1,15 +1,21 @@
-import { PromotionId, PromotionList } from "@promotions/types/promotion";
+import { PromotionId, PromotionList, Promotion } from "@promotions/types/promotion";
 import { log } from "@promotions/lib/log";
 import { Data } from "@promotions/types";
+import prisma from "@promotions/lib/prisma";
 
 export const GetPromotions = async (
-        { request }: Data<PromotionId>,
-        callback: (err: any, response: PromotionList | null) => void
-    ) => {
-        try {
-            callback(null, null);
-        } catch (error) {
-            log.error(error);
-            callback(error, null);
-    }
+	{ request }: Data<PromotionId>,
+	callback: (err: any, response: PromotionList | null) => void
+) => {
+	try {
+		const { id } = request;
+
+		const promotions = await prisma.promotion.findMany() as unknown as Promotion[];
+		const promotionList = {promotions} as PromotionList;
+		
+		callback(null, promotionList);
+	} catch (error) {
+		log.error(error);
+		callback(error, null);
+	}
 };
--- services/promotions/src/lib/prisma.ts
+++ services/promotions/src/lib/prisma.ts
@@ -1,6 +1,4 @@
 import { Prisma, PrismaClient } from "@prisma/client";
-import { createPrismaRedisCache } from "prisma-redis-middleware";
-import { redis } from "@delivery/lib/redis";
 
 let prisma: PrismaClient;
 type GlobalWithPrisma = typeof globalThis & {
@@ -25,20 +23,6 @@ if (process.env.NODE_ENV === "production") {
 
   prisma = (global as GlobalWithPrisma).prisma;
 }
-
-const cacheMiddleware: Prisma.Middleware = createPrismaRedisCache({
-  models: [
-    { model: "DeliveryPerson", cacheTime: 300, cacheKey: "delivery_person" },
-  ],
-  storage: {
-    type: "redis",
-    options: { client: redis, invalidation: { referencesTTL: 300 } },
-  },
-  cacheTime: 300,
-});
-
-//todo: change to $extends when it's available for prisma-redis-middleware
-prisma.$use(cacheMiddleware);
 
 export default prisma;
 
