04eefbc7b7ecee49a672a5fa68a8d68445506596
anatole <anatole.godard@icloud.com>
2023-07-25 00:48:59 +0200

    feat(user): create the user controller

--- services/gateway/src/controller/user/userController.ts
+++ services/gateway/src/controller/user/userController.ts
@@ -1,16 +1,30 @@
 import express, {Request, Response} from 'express';
-import userService from '../../services/user_service';
-import {UserId} from "@order/proto/user_pb";
+import userServiceClient from '../../services/clients/user_client';
+import {
+    changePasswordInput,
+    changeRoleInput,
+    DeleteInput,
+    logInInput,
+    UpdateUserInput,
+    User,
+    UserCreateInput,
+    validateInput
+} from "@order/proto/user_pb";
 import {Empty} from "google-protobuf/google/protobuf/empty_pb";
-
+import {getUser} from "@order/services/user_service";
 
 export const userController = (app: express.Application) => {
     app.get('/api/user/:id', (req: Request, res: Response) => {
         const {id} = req.params;
-        const userId = new UserId();
-        userId.setId(Number(id));
+        try {
+            res.json(getUser(Number(id)));
+        } catch (e: any) {
+            res.json({error: e.message});
+        }
+    });
 
-        userService.getUser(userId, (error, response) => {
+    app.get('/api/user', (req: Request, res: Response) => {
+        userServiceClient.listUser(new Empty(), (error, response) => {
             if (error) {
                 res.json({error: error.message});
             } else {
@@ -19,13 +33,162 @@ export const userController = (app: express.Application) => {
         });
     });
 
-    app.get('/api/user', (req: Request, res: Response) => {
-        userService.listUser(new Empty(), (error, response) => {
+    app.post('/api/user', (req: Request, res: Response) => {
+        const body = req.body;
+        let userInput = new UserCreateInput();
+        try {
+            userInput.setFirstName(body.firstName)
+                .setLastName(body.lastName)
+                .setEmail(body.email)
+                .setPassword(body.password)
+                .setPhone(body.phone)
+                .setMainaddress(body.address)
+                .setRole(body.role);
+
+        } catch (e: any) {
+            res.json({error: e.message});
+        }
+
+        userServiceClient.register(userInput, (error, response) => {
             if (error) {
                 res.json({error: error.message});
             } else {
                 res.json(response.toObject());
             }
         });
+    });
+
+    app.put('/api/user/:id', (req: Request, res: Response) => {
+        const {authorization} = req.headers;
+        if (!authorization) {
+            res.json({error: 'Not authorized'});
+            return;
+        }
+
+        const body = req.body;
+        const userInput = new UpdateUserInput()
+        try {
+            const user = new User().setId(Number(req.params.id))
+                .setFirstName(body.firstName)
+                .setLastName(body.lastName)
+                .setEmail(body.email)
+                .setPhone(body.phone)
+                .setMainaddress(body.address)
+                .setRole(body.role);
+
+            userInput.setUser(user).setToken(authorization);
+
+        } catch (e: any) {
+            res.json({error: e.message});
+        }
+
+        userServiceClient.updateUser(userInput, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.delete('/api/user/:id', (req: Request, res: Response) => {
+        const {id} = req.params;
+        const {authorization} = req.headers;
+        if (!authorization) {
+            res.json({error: 'Not authorized'});
+            return;
+        }
+
+        userServiceClient.deleteUser(new DeleteInput().setUserid(Number(id)).setToken(authorization), (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.post('/api/user/login', (req: Request, res: Response) => {
+        const body = req.body;
+        const inInput = new logInInput().setEmail(body.email).setPassword(body.password);
+
+        userServiceClient.logIn(inInput, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.post('/api/user/validate', (req: Request, res: Response) => {
+        const {authorization} = req.headers;
+        if (!authorization) {
+            res.json({error: 'Not authorized'});
+            return;
+        }
+        const validate = new validateInput().setToken(authorization);
+
+        userServiceClient.validate(validate, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.put('/api/user/:id/password', (req: Request, res: Response) => {
+        const {authorization} = req.headers;
+        if (!authorization) {
+            res.json({error: 'Not authorized'});
+            return;
+        }
+
+        const body = req.body;
+        const updatePasswordInput = new changePasswordInput();
+        try {
+            updatePasswordInput
+                .setToken(authorization)
+                .setOldpassword(body.oldpassword)
+                .setNewpassword(body.password);
+        } catch (e: any) {
+            res.json({error: e.message});
+        }
+
+        userServiceClient.changePassword(updatePasswordInput, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.put('/api/user/:id/role', (req: Request, res: Response) => {
+        const {authorization} = req.headers;
+        if (!authorization) {
+            res.json({error: 'Not authorized'});
+            return;
+        }
+        const body = req.body;
+        const id = req.params.id;
+        const updatePasswordInput = new changeRoleInput();
+        try {
+            updatePasswordInput
+                .setToken(authorization)
+                .setUserid(Number(id))
+                .setRolecode(body.role);
+        } catch (e: any) {
+            res.json({error: e.message});
+        }
+        userServiceClient.changeRole(updatePasswordInput, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+
     });
 }
