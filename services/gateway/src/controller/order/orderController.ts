77ca4191bef9062a9d05598b8a31748458d4a5cc
anatole <anatole.godard@icloud.com>
2023-07-25 00:49:21 +0200

    feat(order): create the order controller

---
+++ services/gateway/src/controller/order/orderController.ts
@@ -0,0 +1,145 @@
+import orderService from '../../services/clients/order_client';
+import express, {Request, Response} from "express";
+import {
+    CreateOrderRequest,
+    DeleteOrderRequest,
+    GetOrderByDeliveryRequest,
+    GetOrderByPaymentRequest,
+    GetOrderRequest,
+    GetOrdersByStatusRequest,
+    GetOrdersByUserRequest,
+    UpdateOrderRequest,
+    UserMinimum
+} from "@order/proto/order_pb";
+import {getUser} from "@order/services/user_service";
+import {User} from "@order/proto/user_pb";
+
+export const orderController = (app: express.Application) => {
+    app.get('/api/order/:id', (req: Request, res: Response) => {
+        const {id} = req.params;
+        const orderId = new GetOrderRequest();
+        orderId.setId(id);
+
+        orderService.getOrder(orderId, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.post('/api/order', (req: Request, res: Response) => {
+        const body = req.body;
+        let user: User | undefined = undefined;
+        try {
+            user = getUser(Number(req.body.userId));
+        } catch (e: any) {
+            res.json({error: e.message});
+        }
+
+        if (!user) {
+            res.json({error: "User not found"});
+            return;
+        }
+
+        const miniUser = new UserMinimum().setId(String(user.getId()))
+            .setEmail(user.getEmail())
+            .setFirstName(user.getFirstName())
+            .setLastName(user.getLastName())
+            .setPhone(user.getPhone());
+
+        let orderInput = new CreateOrderRequest()
+            .setUser(miniUser)
+            .setPaymentId(body.paymentId)
+            .setDeliveryId(body.deliveryId)
+            .setDeliveryType(body.deliveryType)
+            .setBasketSnapshot(body.basketSnapshot)
+            .setRestaurantId(body.restaurantId);
+
+        orderService.createOrder(orderInput, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.get('/api/order/by-user/:userId', (req: Request, res: Response) => {
+        const {userId} = req.params;
+        const orderInput = new GetOrdersByUserRequest().setId(userId);
+        orderService.getOrdersByUser(orderInput, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.post('/api/order/by-status', (req: Request, res: Response) => {
+        const orderInput = new GetOrdersByStatusRequest().setStatus(req.body.status);
+
+        orderService.getOrdersByStatus(orderInput, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.get('/api/order/by-delivery/:deliveryId', (req: Request, res: Response) => {
+        const {deliveryId} = req.params;
+        const orderInput = new GetOrderByDeliveryRequest().setId(deliveryId);
+        orderService.getOrderByDelivery(orderInput, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.get('/api/order/by-payment/:paymentId', (req: Request, res: Response) => {
+        const {paymentId} = req.params;
+        const orderInput = new GetOrderByPaymentRequest().setId(paymentId);
+        orderService.getOrderByPayment(orderInput, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.put('/api/order/:orderId', (req: Request, res: Response) => {
+        const {orderId} = req.params;
+        const orderInput = new UpdateOrderRequest()
+            .setId(orderId)
+            .setStatus(req.body.status)
+            .setDeliveryId(req.body.deliveryId)
+            .setPaymentId(req.body.paymentId)
+            .setRestaurantId(req.body.restaurantId);
+        orderService.updateOrder(orderInput, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.delete('/api/order/:orderId', (req: Request, res: Response) => {
+        const {orderId} = req.params;
+        const orderInput = new DeleteOrderRequest().setId(orderId);
+        orderService.deleteOrder(orderInput, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+}
