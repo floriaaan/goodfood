b7d51adeb8f5b7752e99f0b011ad943de890a601
anatole <anatole.godard@icloud.com>
2023-08-02 13:15:05 +0200

    feat(gateway): add new controller to the server.ts

--- services/gateway/src/utils/swagger.utils.js
+++ services/gateway/src/utils/swagger.utils.js
@@ -1,15 +1,17 @@
-const swaggerAutogen = require('swagger-autogen')();userController
+const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
-    info: {
-        title: 'My API',
-        description: 'Description'
-    },
-    host: 'localhost:3000'
+  info: {
+    title: 'My API',
+    description: 'Description'
+  },
+  host: 'localhost:3000'
 };
 
 const outputFile = './swagger-output.json';
-const routes = ['../controller/user/userController.ts'];
+const routes = ['../controller/user/userController.ts',
+  '../controller/order/orderController.ts',
+  '../controller/product/productController.ts',];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
