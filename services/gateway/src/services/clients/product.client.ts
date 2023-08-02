96336814145e226d860ac68d0d8aca21d06a29b9
anatole <anatole.godard@icloud.com>
2023-08-02 13:14:15 +0200

    feat(gateway): add product controller

---
+++ services/gateway/src/services/clients/product.client.ts
@@ -0,0 +1,15 @@
+import * as grpc from "grpc";
+import {AllergenServiceClient, CategoryServiceClient, ProductServiceClient} from "@gateway/proto/product_grpc_pb";
+
+// TODO: add order env
+export const categoryServiceClient = new CategoryServiceClient(
+    'http://localhost:50007',
+    grpc.credentials.createInsecure());
+
+export const allergenServiceClient = new AllergenServiceClient(
+    'http://localhost:50007',
+    grpc.credentials.createInsecure());
+
+export const productServiceClient = new ProductServiceClient(
+    'http://localhost:50007',
+    grpc.credentials.createInsecure());
