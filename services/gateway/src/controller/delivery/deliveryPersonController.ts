09dd867caff39075622d6876a4eab8ae63606c0c
anatole <anatole.godard@icloud.com>
2023-08-03 17:12:24 +0200

    feat(gateway): create the deliveryPersonController

---
+++ services/gateway/src/controller/delivery/deliveryPersonController.ts
@@ -0,0 +1,88 @@
+import {Application} from "express";
+import {deliveryPersonServiceClient} from "@gateway/services/clients/delivery.client";
+import {DeliveryPerson, DeliveryPersonCreateInput, DeliveryPersonId, Location} from "@gateway/proto/delivery_pb";
+import {Empty} from "google-protobuf/google/protobuf/empty_pb";
+
+export const deliveryPersonController = (app: Application) => {
+    app.get('/api/delivery-person/:id', (req, res) => {
+        const {id} = req.params;
+
+        deliveryPersonServiceClient.getDeliveryPerson(new DeliveryPersonId().setId(id), (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.get('/api/delivery-person', (req, res) => {
+        deliveryPersonServiceClient.listDeliveryPersons(new Empty(), (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.post('/api/delivery-person', (req, res) => {
+        const {lat, lng} = req.body;
+        const location = new Location()
+            .setLatitude(lat)
+            .setLongitude(lng);
+        deliveryPersonServiceClient.listNearDeliveryPersons(location, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.post('/api/delivery-person', (req, res) => {
+        const {firstName, lastName, phone, locationList} = req.body;
+        const deliveryPerson = new DeliveryPersonCreateInput()
+            .setFirstName(firstName)
+            .setLastName(lastName)
+            .setPhone(phone)
+            .setLocationList(locationList);
+        deliveryPersonServiceClient.createDeliveryPerson(deliveryPerson, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.put('/api/delivery-person/:id', (req, res) => {
+        const {id} = req.params;
+        const {firstName, lastName, phone, locationList} = req.body;
+        const deliveryPerson = new DeliveryPerson().setId(id)
+            .setFirstName(firstName)
+            .setLastName(lastName)
+            .setPhone(phone)
+            .setLocationList(locationList);
+
+        deliveryPersonServiceClient.updateDeliveryPerson(deliveryPerson, (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+
+    app.delete('/api/delivery-person/:id', (req, res) => {
+        const {id} = req.params;
+
+        deliveryPersonServiceClient.deleteDeliveryPerson(new DeliveryPersonId().setId(id), (error, response) => {
+            if (error) {
+                res.json({error: error.message});
+            } else {
+                res.json(response.toObject());
+            }
+        });
+    });
+}
