e4d0be029b401a9015a88b72582bd4e9e6aa8b47
anatole <anatole.godard@icloud.com>
2023-08-03 17:12:24 +0200

    feat(gateway): create the deliveryPersonController

---
+++ services/gateway/src/services/clients/delivery.client.ts
@@ -0,0 +1,11 @@
+import services from '../../proto/delivery_grpc_pb';
+import * as grpc from "grpc";
+
+// TODO: add delivery env
+export const deliveryServiceClient = new services.DeliveryServiceClient(
+    'http://localhost:50007',
+    grpc.credentials.createInsecure());
+
+export const deliveryPersonServiceClient = new services.DeliveryPersonServiceClient(
+    'http://localhost:50007',
+    grpc.credentials.createInsecure());
