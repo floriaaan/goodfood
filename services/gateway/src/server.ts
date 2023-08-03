d4bfb3964b672f00720303e2734e1552e8f97a5a
anatole <anatole.godard@icloud.com>
2023-08-03 17:12:24 +0200

    feat(gateway): create the deliveryPersonController

--- services/gateway/src/server.ts
+++ services/gateway/src/server.ts
@@ -4,6 +4,8 @@ import {orderController} from "@gateway/controller/order/orderController";
 import {productController} from "@gateway/controller/product/productController";
 import {categoryController} from "@gateway/controller/product/categoryController";
 import {allergenController} from "@gateway/controller/product/allergenController";
+import {deliveryController} from "@gateway/controller/delivery/deliveryController";
+import {deliveryPersonController} from "@gateway/controller/delivery/deliveryPersonController";
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -22,6 +24,8 @@ orderController(app);
 categoryController(app);
 allergenController(app);
 productController(app);
+deliveryController(app);
+deliveryPersonController(app);
 
 app.listen(PORT, () => {
     console.log(`API Gateway is running on port ${PORT}`);
