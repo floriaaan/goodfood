54ac24628ff793a5df78c9e897c2918609c1a4ca
Florian LEROUX <florian.leroux3@laposte.net>
2023-08-06 14:09:45 +0200

    fix(expo): cant launch expo cli

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,16 +1,8 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
-const { fileURLToPath } = require("url"); // the node package 'url'
-
-function dirname(meta) {
-  return fileURLToPath(meta.url);
-}
-
-// call with import.meta
-const __dirname = dirname(import.meta);
-
 
 /** @type {import('expo/metro-config').MetroConfig} */
+// eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname, {
   // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
