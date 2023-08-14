c4e3777b43085261561dcea8f1c6af8d1c4ca5d8
anatole <anatole.godard@icloud.com>
2023-08-14 21:23:18 +0200

    feat(basket): create the reset basket method

---
+++ services/basket/src/handlers/basket/reset.ts
@@ -0,0 +1,18 @@
+import {log} from "@basket/lib/log";
+import {Data} from "@basket/types";
+import {UserId} from "@basket/types/basket";
+import client from "@basket/lib/redis";
+
+export const Reset = async (
+    {request}: Data<UserId>,
+    callback: (err: any, response: any) => void
+) => {
+    try {
+        const {id} = request;
+        await client.del(`user:${id}`);
+        callback(null, {});
+    } catch (error) {
+        log.error(error);
+        callback(error, null);
+    }
+};
