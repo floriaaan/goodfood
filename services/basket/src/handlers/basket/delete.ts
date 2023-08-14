1152410ecd938708b52333a194bdb5b554646a09
anatole <anatole.godard@icloud.com>
2023-08-14 21:20:37 +0200

    feat(basket): create the delete product method

--- services/basket/src/handlers/basket/delete.ts
+++ services/basket/src/handlers/basket/delete.ts
@@ -1,31 +1,30 @@
 import {log} from "@basket/lib/log";
 import {Data} from "@basket/types";
 import {ProductRequest} from "@basket/types/basket";
-
 import client from "@basket/lib/redis";
 import {RedisBasket} from "@basket/types/redisBasket";
 
-export const AddProduct = async (
-    {request}: Data<ProductRequest>,
+export const DeleteProduct = async (
+    data: Data<ProductRequest>,
     callback: (err: any, response: any) => void
 ) => {
     try {
-        const {product_id, user_id, restaurant_id} = request;
-        const basket = await client.get(`user:${user_id}`);
+        const {request} = data;
+        const {product_id, user_id} = request;
 
+        const basket = await client.get(`user:${user_id}`);
         if (!basket) {
-            await client.set(`user:${user_id}`, JSON.stringify({products_ids: [product_id], restaurant_id}));
-            callback(null, {product_id, user_id});
+            callback(null, "Basket not found");
             return;
         }
 
         const storedBasket: RedisBasket = JSON.parse(basket);
         const newBasket = {
-            products_ids: [...storedBasket.products_ids, product_id],
+            products_ids: storedBasket.products_ids.filter(productId => productId !== product_id),
             restaurant_id: storedBasket.restaurant_id
         };
+        log.info(basket);
         await client.set(`user:${user_id}`, JSON.stringify(newBasket));
-
         callback(null, {user_id, ...newBasket});
     } catch (error) {
         log.error(error);
