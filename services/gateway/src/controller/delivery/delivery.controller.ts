270430d160dcea0451f1b769a3faae0b80fc9f49
anatole <anatole.godard@icloud.com>
2023-08-15 17:55:20 +0200

    feat(global): create docker compose with delivery and the gateway

--- services/gateway/src/controller/delivery/delivery.controller.ts
+++ services/gateway/src/controller/delivery/delivery.controller.ts
@@ -1,6 +1,6 @@
 import {Request, Response, Router} from "express";
 import {deliveryServiceClient} from "@gateway/services/clients/delivery.client";
-import {Delivery, DeliveryCreateInput, DeliveryId, RestaurantId, UserId} from "@gateway/proto/delivery_pb";
+import {Delivery, DeliveryCreateInput, DeliveryId, RestaurantId, Status, UserId} from "@gateway/proto/delivery_pb";
 
 export const deliveryRoutes = Router();
 
@@ -43,19 +43,27 @@ deliveryRoutes.get('/api/delivery/by-user/:id', (req: Request, res: Response) =>
 });
 
 deliveryRoutes.post('/api/delivery', (req: Request, res: Response) => {
-    const {eta, address, status, deliveryPersonId, userId, restaurantId} = req.body;
+    const {eta, address, status, deliveryPersonId, userId, restaurantId}: {
+        eta: string,
+        address: string,
+        status: Status,
+        deliveryPersonId: string,
+        userId: string,
+        restaurantId: string
+    } = req.body;
     const deliveryCreateInput = new DeliveryCreateInput().setEta(eta)
         .setAddress(address)
         .setStatus(status)
         .setDeliveryPersonId(deliveryPersonId)
         .setUserId(userId)
         .setRestaurantId(restaurantId);
-
+    console.log(deliveryCreateInput.toObject());
     deliveryServiceClient.createDelivery(deliveryCreateInput, (error, response) => {
+        console.log(error, response)
         if (error) {
             res.json({error: error.message});
         } else {
-            res.json(response.toObject());
+            res.json(response);
         }
     });
 });
