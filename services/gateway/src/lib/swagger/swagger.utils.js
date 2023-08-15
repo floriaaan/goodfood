1cebdda414349501185f3a4725c4893871ab4a87
anatole <anatole.godard@icloud.com>
2023-08-15 14:29:12 +0200

    feat(gateway): move the swagger lib

--- services/gateway/src/lib/swagger/swagger.utils.js
+++ services/gateway/src/lib/swagger/swagger.utils.js
@@ -9,7 +9,7 @@ const doc = {
 };
 
 const outputFile = './swagger-output.json';
-const routes = ['../server.ts',];
+const routes = ['../../server.ts',];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
