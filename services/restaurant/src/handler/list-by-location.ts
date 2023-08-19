78a0a16a3eef76007f308e996c8c0afb68cf023e
Florian LEROUX <florian.leroux3@laposte.net>
2023-08-19 17:31:57 +0200

    feat(location): implement list by location

--- services/restaurant/src/handler/list-by-location.ts
+++ services/restaurant/src/handler/list-by-location.ts
@@ -6,6 +6,23 @@ import {
   Restaurant,
   RestaurantList,
 } from "@restaurant/types/restaurant";
+import geolib from "geolib";
+
+interface RestaurantWithDistance extends Restaurant {
+  distance: number;
+}
+
+export const sortRestaurantsByDistance = (
+  restaurants: Restaurant[],
+  location: [number, number]
+): RestaurantWithDistance[] => {
+  return restaurants
+    .map((restaurant) => ({
+      ...restaurant,
+      distance: geolib.getDistance(location, restaurant.location),
+    }))
+    .sort((a, b) => a.distance - b.distance);
+};
 
 export const GetRestaurantsByLocation = async (
   { request }: Data<ByLocationInput>,
@@ -14,13 +31,11 @@ export const GetRestaurantsByLocation = async (
   try {
     const { location } = request;
 
-    const restaurants = (await prisma.restaurant.findMany({
-      where: {
-        location: {
-          equals: location,
-        },
-      },
-    })) as unknown as Restaurant[];
+    const all = (await prisma.restaurant.findMany()) as unknown as Restaurant[];
+
+    const restaurants = sortRestaurantsByDistance(all, location)
+      .map(({ distance, ...restaurant }) => restaurant as Restaurant)
+      .filter((_, i) => i < 10);
 
     callback(null, { restaurants });
   } catch (error) {
