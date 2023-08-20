4a120dcdc1af7dfabcf5fedde683b31b1f1d8ecc
Florian LEROUX <florian.leroux3@laposte.net>
2023-08-20 18:08:23 +0200

    Revert "feat(middleware): add auth middleware"
    This reverts commit 07752cbf7c5278b75028bfc86d78bdf06707c0c4.

--- services/gateway/src/services/user.service.ts
+++ services/gateway/src/services/user.service.ts
@@ -1,59 +1,16 @@
-import { User, UserId, validateInput } from "@gateway/proto/user_pb";
-import { userServiceClient } from "@gateway/services/clients/user.client";
+import {User, UserId} from "@gateway/proto/user_pb";
+import {userServiceClient} from "@gateway/services/clients/user.client";
 
 export const getUser = (id: number): User | undefined => {
-  const userId = new UserId();
-  userId.setId(Number(id));
-  let user: User | undefined = undefined;
-  userServiceClient.getUser(userId, (error, response) => {
-    if (error) {
-      throw Error(error.message);
-    } else {
-      user = response.getUser();
-    }
-  });
-  return user;
-};
-
-export const getUserIdFromToken = (token: string): number | undefined => {
-  const userToken = new validateInput();
-  userToken.setToken(token);
-  let user: number | undefined = undefined;
-  userServiceClient.validate(userToken, (error, response) => {
-    if (error) {
-      throw Error(error.message);
-    } else {
-      user = response.getUserid();
-    }
-  });
-  return user;
-};
-
-export const user_service_promises = {
-  getUser: (id: number): Promise<User | undefined> => {
     const userId = new UserId();
     userId.setId(Number(id));
-    return new Promise((resolve, reject) => {
-      userServiceClient.getUser(userId, (error, response) => {
+    let user: User | undefined = undefined;
+    userServiceClient.getUser(userId, (error, response) => {
         if (error) {
-          reject(error);
+            throw Error(error.message);
         } else {
-          resolve(response.getUser());
+            user = response.getUser()
         }
-      });
     });
-  },
-  getUserIdFromToken: (token: string): Promise<number | undefined> => {
-    const userToken = new validateInput();
-    userToken.setToken(token);
-    return new Promise((resolve, reject) => {
-      userServiceClient.validate(userToken, (error, response) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(response.getUserid());
-        }
-      });
-    });
-  },
-};
+    return user;
+}
