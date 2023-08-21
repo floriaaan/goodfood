629d3805825d13f19c32261a8381f09d821e4cdb
anatole <anatole.godard@icloud.com>
2023-08-21 15:51:37 +0200

    feat(gateway): add mainAddress routes in the server.ts

--- services/gateway/src/controller/user/mainAddress.controller.ts
+++ services/gateway/src/controller/user/mainAddress.controller.ts
@@ -2,9 +2,9 @@ import {Request, Response, Router} from 'express';
 import {mainAddressServiceClient} from "@gateway/services/clients/user.client";
 import {MainAddress, MainAddressId, MainAddressUpdateInput} from "@gateway/proto/user_pb";
 
-export const userRoutes = Router();
+export const mainAddressRoutes = Router();
 
-userRoutes.get('/api/user/main-address/:id', async (req: Request, res: Response) => {
+mainAddressRoutes.get('/api/user/main-address/:id', async (req: Request, res: Response) => {
     const {id} = req.params;
     mainAddressServiceClient.getMainAddress(new MainAddressId().setId(Number(id)), (err, response) => {
         if (err) {
@@ -14,7 +14,7 @@ userRoutes.get('/api/user/main-address/:id', async (req: Request, res: Response)
     });
 });
 
-userRoutes.put('/api/user/main-address/:id', async (req: Request, res: Response) => {
+mainAddressRoutes.put('/api/user/main-address/:id', async (req: Request, res: Response) => {
     const {authorization} = req.headers;
     if (!authorization) {
         res.json({error: 'Not authorized'});
