206812f7ea3fc5aff8f0986b451b5bde4cdaf43a
anatole <anatole.godard@icloud.com>
2023-08-21 18:33:26 +0200

    feat(gateway): add delivery schema in the swagger

--- services/gateway/src/lib/swagger/swagger.utils.js
+++ services/gateway/src/lib/swagger/swagger.utils.js
@@ -5,7 +5,17 @@ const doc = {
     title: 'The Gateway API',
     description: 'The Gateway API for the Microservices',
   },
-  host: `localhost:${process.env.GATEWAY_PORT || 50000}`
+  host: `localhost:${process.env.GATEWAY_PORT || 50000}`,
+  definitions: {
+    Status: {
+      '@enum': [
+        "PENDING",
+        "IN_PROGRESS",
+        "FULFILLED",
+        "REJECTED"
+      ]
+    }
+  }
 };
 
 const outputFile = './swagger-output.json';
