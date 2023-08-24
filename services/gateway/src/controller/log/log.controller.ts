3844d0c0c6ed4f8d5d72af774501dbc95b17172d
Florian LEROUX <florian.leroux3@laposte.net>
2023-08-24 16:32:29 +0200

    feat(log): add auth checkguards in routes

--- services/gateway/src/controller/log/log.controller.ts
+++ services/gateway/src/controller/log/log.controller.ts
@@ -1,29 +1,44 @@
-import express, {Router} from "express";
-import {logServiceClient} from "@gateway/services/clients/log.client";
-import {Empty} from "google-protobuf/google/protobuf/empty_pb";
-import {GetLogRequest} from "@gateway/proto/log_pb";
+import { Request, Response, Router } from "express";
+import { logServiceClient } from "@gateway/services/clients/log.client";
+import { Empty } from "google-protobuf/google/protobuf/empty_pb";
+import { GetLogRequest } from "@gateway/proto/log_pb";
+import { withCheck } from "@gateway/middleware/auth";
 
 export const logRoutes = Router();
 
-logRoutes.get('/api/log', (req: express.Request, res: express.Response) => {
-
-    logServiceClient.listLog(new Empty(), (error, response) => {
-        if (error) {
-            res.status(500).send({error: error.message});
-        } else {
-            res.json(response.toObject());
+logRoutes.get("/api/log", withCheck({ role: "ADMIN" }), (_: Request, res: Response) => {
+  /* #swagger.parameters['headers'] = {
+        in: 'header',
+        required: true,
+        schema: {
+            Authorization: "Bearer <token>"
         }
-    });
+    } */
+  logServiceClient.listLog(new Empty(), (error, response) => {
+    if (error) return res.status(500).send({ error: error.message });
+    else return res.status(200).json(response.toObject());
+  });
 });
 
-logRoutes.get('/api/log/:id', (req: express.Request, res: express.Response) => {
-    const {id} = req.params
-
-    logServiceClient.getLog(new GetLogRequest().setId(Number(id)), (error, response) => {
-        if (error) {
-            res.status(500).send({error: error.message});
-        } else {
-            res.json(response.toObject());
+logRoutes.get("/api/log/:id", withCheck({ role: "ADMIN" }), (req: Request, res: Response) => {
+  /* #swagger.parameters['query'] = {
+        in: 'query',
+        required: true,
+        schema: {
+            id: "delivery_person_id:1"
         }
-    });
+    }
+    #swagger.parameters['headers'] = {
+        in: 'header',
+        required: true,
+        schema: {
+            Authorization: "Bearer <token>"
+        }
+    } */
+  const { id } = req.query as { id: string };
+
+  logServiceClient.getLog(new GetLogRequest().setId(Number(id)), (error, response) => {
+    if (error) return res.status(500).send({ error: error.message });
+    else return res.status(200).json(response.toObject());
+  });
 });
