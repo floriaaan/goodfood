372e1abec281f8ebddb3735317f8f16715c2fca1
Florian LEROUX <florian.leroux3@laposte.net>
2023-08-25 11:31:23 +0200

    feat(payment): add auth checkguards in routes

--- services/gateway/src/controller/payment/payment.controller.ts
+++ services/gateway/src/controller/payment/payment.controller.ts
@@ -1,29 +1,80 @@
-import {Request, Response, Router} from "express";
-import {GetPaymentRequest, GetPaymentsByUserRequest} from "@gateway/proto/payment_pb";
-import {paymentServiceClient} from "@gateway/services/clients/payment.client";
+import { Request, Response, Router } from "express";
+import { GetPaymentRequest, GetPaymentsByUserRequest, Payment } from "@gateway/proto/payment_pb";
+import { paymentServiceClient } from "@gateway/services/clients/payment.client";
+import { getUserIdFromToken } from "@gateway/services/user.service";
+import { check } from "@gateway/middleware/auth";
 
 export const paymentRoutes = Router();
 
-paymentRoutes.get('/api/payment/:id', (req: Request, res: Response) => {
-    const {id} = req.params;
-
-    paymentServiceClient.getPayment(new GetPaymentRequest().setId(id), (error, response) => {
-        if (error) {
-            res.status(500).send({error: error.message});
-        } else {
-            res.json(response.toObject());
+paymentRoutes.get("/api/payment/:id", async (req: Request, res: Response) => {
+  /* #swagger.parameters['id'] = {
+        in: 'path',
+        required: true,
+        schema: {
+            id: "payment_id:1"
         }
+    } 
+    #swagger.parameters['headers'] = {
+        in: 'header',
+        required: true,
+        schema: {
+            Authorization: "Bearer <token>"
+        }
+    } */
+
+  // Auth check and :id check ---
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: "Unauthorized" });
+  const token = authorization.split("Bearer ")[1];
+  const userId = await getUserIdFromToken(token);
+  if (!userId) return res.status(401).json({ message: "Unauthorized" });
+  // ----------------------------
+  const { id } = req.params;
+
+  try {
+    const payment: Payment.AsObject = await new Promise((resolve, reject) => {
+      paymentServiceClient.getPayment(new GetPaymentRequest().setId(id), (error, response) => {
+        if (error) reject(error);
+        else resolve(response.toObject());
+      });
     });
+
+    if (payment.userId !== userId.toString() || (await check(token, { role: "ADMIN" })))
+      return res.status(401).json({ message: "Unauthorized" });
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
 });
 
-paymentRoutes.get('/api/payment/by-user/:userId', (req: Request, res: Response) => {
-    const {id} = req.params;
-
-    paymentServiceClient.getPaymentsByUser(new GetPaymentsByUserRequest().setId(id), (error, response) => {
-        if (error) {
-            res.status(500).send({error: error.message});
-        } else {
-            res.json(response.toObject());
+paymentRoutes.get("/api/payment/by-user/:userId", async (req: Request, res: Response) => {
+  /* #swagger.parameters['userId'] = {
+        in: 'path',
+        required: true,
+        schema: {
+            userId: "user_id:1"
         }
-    });
+    } 
+    #swagger.parameters['headers'] = {
+        in: 'header',
+        required: true,
+        schema: {
+            Authorization: "Bearer <token>"
+        }
+    } */
+
+  // Auth check and :id check ---
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: "Unauthorized" });
+  const token = authorization.split("Bearer ")[1];
+  const userId = await getUserIdFromToken(token);
+  if (!userId) return res.status(401).json({ message: "Unauthorized" });
+  // ----------------------------
+  const { id } = req.params;
+  if (!(await check(token, { role: "ADMIN" })) || !(await check(token, { id: Number(id) })))
+    return res.status(401).json({ message: "Unauthorized" });
+
+  paymentServiceClient.getPaymentsByUser(new GetPaymentsByUserRequest().setId(id), (error, response) => {
+    if (error) return res.status(500).send({ error });
+    else return res.status(200).json(response.toObject());
+  });
 });
