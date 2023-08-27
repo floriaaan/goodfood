c55084ce5e2d5bff129e5e68ea47890f582eef94
anatole <anatole.godard@icloud.com>
2023-08-27 21:04:15 +0200

    fix(product): fix list method

--- services/product/src/handler/Product/list.ts
+++ services/product/src/handler/Product/list.ts
@@ -1,21 +1,27 @@
-import { ProductList, RestaurantId } from "@product/types/Product";
-import { Data } from "@product/types";
-import { log } from "@product/lib/log";
+import {Product, ProductList, RestaurantId} from "@product/types/Product";
+import {Data} from "@product/types";
+import {log} from "@product/lib/log";
 import prisma from "@product/lib/prisma";
-import { ServerErrorResponse } from "@grpc/grpc-js";
+import {ServerErrorResponse} from "@grpc/grpc-js";
 
 export const getProductList = async (
-	{ request }: Data<RestaurantId>,
-	callback: (err: ServerErrorResponse | any, response: ProductList | null) => void
+    {request}: Data<RestaurantId>,
+    callback: (err: ServerErrorResponse | any, response: ProductList | null) => void
 ) => {
-	try {
-		const { id } = request;
+    try {
+        const {id} = request;
 
-		const products = await prisma.product.findMany({ where : {restaurant_id: id} }) as unknown as ProductList;
+        const products = await prisma.product.findMany({
+            where: {restaurant_id: id},
+            include: {
+                categories: true,
+                allergens: true,
+            }
+        }) as unknown as Product[];
 
-		callback(null, products);
-	} catch (error: ServerErrorResponse | any) {
-		log.error(error);
-		callback(error, null);
-	}
+        callback(null, {products});
+    } catch (error: ServerErrorResponse | any) {
+        log.error(error);
+        callback(error, null);
+    }
 };
