6debd4112903684d040a923a02bdfde0e3a36d8e
Florian LEROUX <florian.leroux3@laposte.net>
2023-08-27 12:36:52 +0200

    feat(mainAddress): add auth checkguards in routes

--- services/gateway/src/controller/user/mainAddress.controller.ts
+++ services/gateway/src/controller/user/mainAddress.controller.ts
@@ -1,46 +1,72 @@
 import { Request, Response, Router } from "express";
 import { mainAddressServiceClient } from "@gateway/services/clients/user.client";
 import { MainAddress, MainAddressId, MainAddressUpdateInput } from "@gateway/proto/user_pb";
+import { getUserIdFromToken } from "@gateway/services/user.service";
+import { check } from "@gateway/middleware/auth";
 
 export const mainAddressRoutes = Router();
 
 mainAddressRoutes.get("/api/user/main-address/:id", async (req: Request, res: Response) => {
-  /* #swagger.parameters['id'] = {
-           in: 'path',
-           required: true,
-           type: 'integer'
-     }*/
+  /*  #swagger.parameters['id'] = {
+        in: 'path',
+        required: true,
+        type: 'integer'
+      }
+      #swagger.parameters['authorization'] = {
+        in: 'header',
+        required: true,
+        type: 'string'
+      } 
+    */
+
+  // Auth check and :id check ---
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: "Unauthorized" });
+  const token = authorization.split("Bearer ")[1];
+  const userId = await getUserIdFromToken(token);
+  if (!userId) return res.status(401).json({ message: "Unauthorized" });
+  if (!(await check(token, { id: userId }))) return res.status(401).json({ message: "Unauthorized" });
+  // ----------------------------
+
   const { id } = req.params;
-  mainAddressServiceClient.getMainAddress(new MainAddressId().setId(Number(id)), (err, response) => {
-    if (err) {
-      return res.status(500).json({ message: err.message });
-    }
-    return res.json(response.toObject());
+  mainAddressServiceClient.getMainAddress(new MainAddressId().setId(Number(id)), (error, response) => {
+    if (error) return res.status(500).send({ error });
+    else return res.status(200).json(response.toObject());
   });
 });
 
 mainAddressRoutes.put("/api/user/main-address/:id", async (req: Request, res: Response) => {
-  /* #swagger.parameters['id'] = {
-           in: 'path',
-           required: true,
-           type: 'integer'
-     }
-     #swagger.parameters['body'] = {
-            in: 'body',
-            required: true,
-            schema: {
-                country: "France",
-                zipCode: "76000",
-                street: "7 rue de la paix",
-                lat: 49.443232,
-                lng: 1.099971
-            }
-    } */
+  /*  #swagger.parameters['id'] = {
+        in: 'path',
+        required: true,
+        type: 'integer'
+      }
+      #swagger.parameters['body'] = {
+        in: 'body',
+        required: true,
+        schema: {
+            country: "France",
+            zipCode: "76000",
+            street: "7 rue de la paix",
+            lat: 49.443232,
+            lng: 1.099971
+        }
+      } 
+      #swagger.parameters['authorization'] = {
+        in: 'header',
+        required: true,
+        type: 'string'
+      } 
+    */
+
+  // Auth check and :id check ---
   const { authorization } = req.headers;
-  if (!authorization) {
-    res.json({ error: "Not authorized" });
-    return;
-  }
+  if (!authorization) return res.status(401).json({ message: "Unauthorized" });
+  const token = authorization.split("Bearer ")[1];
+  const userId = await getUserIdFromToken(token);
+  if (!userId) return res.status(401).json({ message: "Unauthorized" });
+  if (!(await check(token, { id: userId }))) return res.status(401).json({ message: "Unauthorized" });
+  // ----------------------------
 
   const { id } = req.params;
   const { country, zipCode, street, lat, lng } = req.body;
@@ -53,10 +79,8 @@ mainAddressRoutes.put("/api/user/main-address/:id", async (req: Request, res: Re
     .setLng(lng);
 
   const mainAddressUpdateInput = new MainAddressUpdateInput().setToken(authorization).setMainaddress(address);
-  mainAddressServiceClient.updateMainAddress(mainAddressUpdateInput, (err, response) => {
-    if (err) {
-      return res.status(500).json({ message: err.message });
-    }
-    return res.json(response.toObject());
+  mainAddressServiceClient.updateMainAddress(mainAddressUpdateInput, (error, response) => {
+    if (error) return res.status(500).send({ error });
+    else return res.status(200).json(response.toObject());
   });
 });
