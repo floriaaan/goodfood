e226062436fb36dfba1742651fb7cf82bead2872
Florian LEROUX <florian.leroux3@laposte.net>
2023-08-30 13:45:59 +0200

    init(web): add next.js project in /apps/web

---
+++ apps/web/next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  headers: () => [
+    // HSTS
+    {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "Strict-Transport-Security",
+          value: "max-age=63072000; includeSubDomains; preload",
+        },
+      ],
+    },
+  ],
+};
+
+module.exports = nextConfig;
