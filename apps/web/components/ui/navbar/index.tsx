358f680d61a144928315f1703db72a750eccf088
Florian LEROUX <florian.leroux3@laposte.net>
2023-08-31 12:00:55 +0200

    feat(home): add navbar and heading section

---
+++ apps/web/components/ui/navbar/index.tsx
@@ -0,0 +1,22 @@
+import { Logo } from "@/components/ui/Logo";
+import { DropdownList } from "@/components/ui/navbar/dropdown/list";
+import { MenuDropdown } from "@/components/ui/navbar/dropdown/menu";
+import { Location } from "@/components/ui/navbar/location";
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <nav className="inline-flex w-screen items-center justify-between border-b px-8 py-4">
+      <Link id="nav-logo" data-testid="nav-logo" className="lg:w-64" href={"/"}>
+        <Logo className="h-12" />
+      </Link>
+      <Location className="hidden lg:inline-flex" />
+      <>
+        <div className="md:hidden">
+          <MenuDropdown />
+        </div>
+        <DropdownList className="hidden md:inline-flex" />
+      </>
+    </nav>
+  );
+};
