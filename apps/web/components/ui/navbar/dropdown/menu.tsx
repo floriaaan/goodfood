6d775b3a9e30326ef7af8da8cac1cc09ddd27e6c
Florian LEROUX <florian.leroux3@laposte.net>
2023-08-31 12:00:55 +0200

    feat(home): add navbar and heading section

---
+++ apps/web/components/ui/navbar/dropdown/menu.tsx
@@ -0,0 +1,9 @@
+import { MdMenu } from "react-icons/md";
+
+export const MenuDropdown = () => {
+  return (
+    <button className="gf_shadow flex h-10 w-10 shrink-0 items-center justify-center border border-black">
+      <MdMenu className="h-5 w-5" />
+    </button>
+  );
+};
