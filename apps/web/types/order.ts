fddc0b68795a4fc8707542cf53d10411c02a0e37
Florian LEROUX <florian.leroux3@laposte.net>
2023-09-01 14:56:31 +0200

    feat(init): add types and tmp data, init shadcn/ui components

---
+++ apps/web/types/order.ts
@@ -0,0 +1,40 @@
+import { Status } from "@/types/global";
+import { Delivery } from "@/types/delivery";
+import { Payment } from "@/types/payment";
+
+export type UserMinimum = {
+  id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+};
+
+export type Basket = {
+  string: string;
+  json?: string | object | any;
+  total: number;
+};
+
+export enum DeliveryType {
+  TAKEAWAY = "TAKEAWAY",
+  DELIVERY = "DELIVERY",
+}
+
+export type Order = {
+  id: string;
+  delivery_type: DeliveryType;
+  restaurant_id: string;
+  user: UserMinimum;
+  basket_snapshot: Basket;
+  status: Status;
+
+  payment_id: string;
+  payment: Payment;
+
+  delivery_id: string;
+  delivery: Delivery;
+
+  created_at: Date | string;
+  updated_at: Date | string;
+};
