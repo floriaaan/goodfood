6fc662a0cddd8f1dbbff08d5cf4615e09abd446f
Florian LEROUX <florian.leroux3@laposte.net>
2023-09-03 23:15:04 +0200

    ref: change behavior of components

--- apps/web/components/basket/promotion.tsx
+++ apps/web/components/basket/promotion.tsx
@@ -1,8 +1,13 @@
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+
 export const BasketPromotion = () => {
   return (
-    <label className="inline-flex items-center justify-between p-2 my-2">
-      <span className="text-xs font-light">{"J'ai un code promo ou code de parrainage"}</span>
-      <input type="checkbox" className=""></input>
-    </label>
+    <div className="my-2 inline-flex items-center justify-between p-2">
+      <Label htmlFor="promo" className="text-xs font-light">
+        {"J'ai un code promo ou code de parrainage"}
+      </Label>
+      <Switch id="promo" />
+    </div>
   );
 };
