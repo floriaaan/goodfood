5e0cd83fea76876f67a4244b780ff54f4650aa3a
Florian LEROUX <florian.leroux3@laposte.net>
2023-09-03 23:14:27 +0200

    feat(ui): add shadcn components

--- apps/web/components/ui/button/solid.tsx
+++ apps/web/components/ui/button/solid.tsx
@@ -1,7 +1,12 @@
+import { cn } from "@/lib/utils";
+
 export const SolidButton = ({ children, className, ...props }: { children: React.ReactNode; className?: string }) => {
   return (
     <button
-      className={`inline-flex items-center justify-center w-full font-bold  focus:outline-none focus:ring-2 duration-200 uppercase text-sm gap-x-2 p-3 focus:ring-offset-2 ${className}`}
+      className={cn(
+        `inline-flex w-full items-center justify-center gap-x-2  p-3 text-sm font-bold uppercase duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2`,
+        className,
+      )}
       {...props}
     >
       {children}
