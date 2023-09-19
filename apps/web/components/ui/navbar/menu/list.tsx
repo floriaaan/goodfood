2799ca04dafc869ac6124397ff69c0b1963dd0db
Florian LEROUX <florian.leroux3@laposte.net>
2023-09-19 17:27:28 +0200

    feat(navbar): add more consistency between sheets

--- apps/web/components/ui/navbar/menu/list.tsx
+++ apps/web/components/ui/navbar/menu/list.tsx
@@ -1,6 +1,5 @@
 import { BasketDropdown } from "@/components/ui/navbar/menu/basket";
 import { LocationDropdown } from "@/components/ui/navbar/menu/location";
-import { UserDropdown } from "@/components/ui/navbar/menu/user";
 import { cn } from "@/lib/utils";
 
 export const DropdownList = ({ className }: { className?: string }) => {
@@ -8,7 +7,6 @@ export const DropdownList = ({ className }: { className?: string }) => {
     <div className={cn("items-center justify-end gap-x-4 lg:w-64", className)}>
       <LocationDropdown />
       <BasketDropdown />
-      <UserDropdown />
     </div>
   );
 };
