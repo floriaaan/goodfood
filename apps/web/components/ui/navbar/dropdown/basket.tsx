39962b7496345290fbdd0064f404d379eb0fd64d
Florian LEROUX <florian.leroux3@laposte.net>
2023-09-19 15:56:46 +0200

    fix(react issues): fix hydrating issues

--- apps/web/components/ui/navbar/dropdown/basket.tsx
+++
@@ -1,9 +0,0 @@
-import { MdOutlineShoppingBasket } from "react-icons/md";
-
-export const BasketDropdown = () => {
-  return (
-    <button className="gf_shadow flex h-10 w-10 shrink-0 items-center justify-center border border-black">
-      <MdOutlineShoppingBasket className="h-5 w-5" />
-    </button>
-  );
-};
--- apps/web/components/ui/navbar/dropdown/location.tsx
+++
@@ -1,9 +0,0 @@
-import { MdOutlineLocationOn } from "react-icons/md";
-
-export const LocationDropdown = () => {
-  return (
-    <button className="gf_shadow flex h-10 w-10 shrink-0 lg:hidden items-center justify-center border border-black">
-      <MdOutlineLocationOn className="h-5 w-5" />
-    </button>
-  );
-};
