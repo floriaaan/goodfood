4c5f039e47f558c7dff3cfd54ec885a18dc5cec2
Florian LEROUX <florian.leroux3@laposte.net>
2023-09-19 15:56:46 +0200

    fix(react issues): fix hydrating issues

---
+++ apps/web/components/ui/navbar/menu/location.tsx
@@ -0,0 +1,14 @@
+import { Location } from "@/components/ui/navbar/location";
+import { MdOutlineLocationOn } from "react-icons/md";
+
+export const LocationDropdown = () => {
+  return (
+    <Location
+      trigger={
+        <div className="gf_shadow flex h-10 w-10 shrink-0 items-center justify-center border border-black lg:hidden">
+          <MdOutlineLocationOn className="h-5 w-5" />
+        </div>
+      }
+    />
+  );
+};
