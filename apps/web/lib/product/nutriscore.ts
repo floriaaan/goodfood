b974c70e0424ff2562924624382befa6c76ec680
Florian LEROUX <florian.leroux3@laposte.net>
2023-09-19 17:26:49 +0200

    feat(nutriscore): add svg for nutriscore

--- apps/web/lib/product/nutriscore.ts
+++ apps/web/lib/product/nutriscore.ts
@@ -3,15 +3,15 @@ import { Product } from "@/types/product";
 export const getNutriscoreImageUrl = ({ nutriscore }: Product) => {
   switch (nutriscore) {
     case 1:
-      return "/images/nutriscore/nutriscore-a.png";
+      return "/images/nutriscore/nutriscore-a.svg";
     case 2:
-      return "/images/nutriscore/nutriscore-b.png";
+      return "/images/nutriscore/nutriscore-b.svg";
     case 3:
-      return "/images/nutriscore/nutriscore-c.png";
+      return "/images/nutriscore/nutriscore-c.svg";
     case 4:
-      return "/images/nutriscore/nutriscore-d.png";
+      return "/images/nutriscore/nutriscore-d.svg";
     case 5:
-      return "/images/nutriscore/nutriscore-e.png";
+      return "/images/nutriscore/nutriscore-e.svg";
     default:
       return "";
   }
