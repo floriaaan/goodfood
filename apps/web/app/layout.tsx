53b153cdab049a189a68a86558aba13682265f0d
Florian LEROUX <florian.leroux3@laposte.net>
2023-09-19 11:38:19 +0200

    feat(basket): add basket behavior

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fr">
+    <html lang="fr" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
--- apps/web/components/basket/product.tsx
+++ apps/web/components/basket/product.tsx
@@ -1,39 +1,47 @@
 import { ProductSheetContent } from "@/components/product/sheet-content";
 import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+import { useBasket } from "@/hooks/useBasket";
 import { Product } from "@/types/product";
 import Image from "next/image";
 import { MdAdd, MdDelete, MdRemove } from "react-icons/md";
 
 export const ProductBasketItem = (product: Product) => {
+  const { addProduct, removeProduct, basket } = useBasket();
   return (
     <Sheet key={product.id}>
-      <div className="inline-flex h-20 w-full p-1 xl:h-24">
+      <div className="inline-flex h-16 w-full p-1 xl:h-20">
         <SheetTrigger asChild>
           <Image
-            className="h-full w-24 shrink-0 cursor-pointer border border-black object-cover xl:w-32"
+            className="h-full w-12 shrink-0 cursor-pointer border border-black object-cover xl:w-24"
             width={256}
             height={192}
             src={product.image}
             alt={product.name}
           />
         </SheetTrigger>
-        <div className="flex h-full w-full grow flex-col justify-between p-2">
-          <span className="text-sm font-bold">{product.name}</span>
-          <div className="inline-flex w-full items-center justify-between">
-            <div className="inline-flex items-center gap-x-1">
-              <button className="flex h-8 w-8 items-center justify-center border bg-gray-100">
-                <MdRemove className="h-4 w-4" />
+        <div className="flex h-full w-full grow flex-col justify-between px-2 pt-1">
+          <span className="line-clamp-2 h-full grow text-sm ">{product.name}</span>
+          <div className="inline-flex w-full items-end justify-between">
+            <div className="inline-flex items-center">
+              <button
+                className="flex h-6 w-6 items-center justify-center border bg-gray-100"
+                onClick={() => removeProduct(product.id, 1)}
+              >
+                {basket[product.id] === 1 ? <MdDelete className="h-4 w-4" /> : <MdRemove className="h-4 w-4" />}
               </button>
-              <span className="flex h-8 w-8 items-center justify-center border bg-gray-100 text-xs font-bold">1</span>
-              <button className="flex h-8 w-8 items-center justify-center border bg-gray-100">
+              <span className="flex h-6 w-6 items-center justify-center border bg-gray-100 text-xs font-bold">
+                {basket[product.id]}
+              </span>
+              <button
+                className="flex h-6 w-6 items-center justify-center border bg-gray-100"
+                onClick={() => addProduct(product.id, 1)}
+              >
                 <MdAdd className="h-4 w-4" />
               </button>
             </div>
             <div className="inline-flex items-center gap-x-1">
-              <button className="flex h-8 w-8 items-center justify-center border bg-gray-100">
-                <MdDelete className="h-4 w-4" />
-              </button>
-              <span className="flex h-8 items-center justify-center border bg-gray-200 px-2 text-sm font-bold text-black">
+            
+              <span className="flex h-6 items-center justify-center border bg-gray-200 px-2 text-sm font-bold text-black">
                 {product.price.toFixed(2).replace(".", "€")}
               </span>
             </div>
