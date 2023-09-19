b7a46d82d81115e8d3e774a9459ff43fbb862e28
Florian LEROUX <florian.leroux3@laposte.net>
2023-09-19 17:27:28 +0200

    feat(navbar): add more consistency between sheets

--- apps/web/app/(user)/(home)/page.tsx
+++ apps/web/app/(user)/(home)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <Navbar />
       <RatingSection />
-      <div className="relative p-2 has-background-grid">
+      <div className="relative p-2 pt-6 has-background-grid">
         <main className="sticky mx-auto grid shadow-xl max-w-7xl gap-2 border bg-white border-gray-100 p-2 lg:grid-cols-3">
           <Catalog />
           <div className="hidden lg:block">
