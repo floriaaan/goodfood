c310d9eeabb775c3847600dfeb2dab35178ff981
Florian LEROUX <florian.leroux3@laposte.net>
2023-09-26 14:58:25 +0200

    ref(location): change location behavior

--- apps/web/components/location/trigger.tsx
+++ apps/web/components/location/trigger.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { restaurantList } from "@/constants/data";
-import { useAuth } from "@/hooks/useAuth";
 import { useBasket } from "@/hooks/useBasket";
 import { cn } from "@/lib/utils";
 import { MdArrowForwardIos, MdOutlineLocationOn } from "react-icons/md";
 
 export const LocationFullTrigger = ({ className }: { className?: string }) => {
-  const { selectedRestaurantId } = useBasket();
-  const { user } = useAuth();
-  const { mainaddress } = user || {};
-  const { street, zipcode, city, country } = mainaddress || {};
+  const { selectedRestaurantId, address } = useBasket();
+  const { street, zipcode, city, country } = address || {};
+
+  const isAddressDefined = street && zipcode && city && country;
+  const address_displayed = isAddressDefined ? `${street}, ${zipcode} ${city}, ${country}` : "Ajouter une adresse";
 
   //todo: calc time to deliver between restaurant and user (use geolib ? or gmaps api ?)
   const eta = "(12:15 - 12:35)";
@@ -24,25 +24,23 @@ export const LocationFullTrigger = ({ className }: { className?: string }) => {
     >
       <div className="relative">
         <MdOutlineLocationOn className="h-7 w-7 shrink-0" />
-        {!selectedRestaurantId && (
+        {!selectedRestaurantId || !isAddressDefined && (
           <>
             <span className="absolute right-0 top-0 h-2 w-2 animate-ping rounded-full bg-red-600"></span>
             <span className="absolute right-0 top-0 h-2 w-2 rounded-full bg-red-600"></span>
           </>
         )}
       </div>
-      {selectedRestaurantId ? (
-        <div className="flex max-w-sm grow flex-col gap-y-0.5">
-          <span className="text-xs font-bold">{`${street}, ${zipcode} ${city}, ${country}`}</span>
-          <span className="text-xs">{`${// todo: link to restaurant list store
-          restaurantList.find(({ id }) => id === selectedRestaurantId)?.name} - ${eta}`}</span>
-        </div>
-      ) : (
-        <div className="flex max-w-sm grow flex-col gap-y-0.5">
-          <span className="text-xs font-bold">{`${street}, ${zipcode} ${city}, ${country}`}</span>
-          <span className="text-xs">Choisir un restaurant</span>
-        </div>
-      )}
+      <div className="flex max-w-sm grow flex-col gap-y-0.5">
+        <span className="text-xs font-bold">{address_displayed}</span>
+        <span className="text-xs">
+          {selectedRestaurantId
+            ? `${// todo: link to restaurant list store
+              restaurantList.find(({ id }) => id === selectedRestaurantId)?.name} - ${eta}`
+            : "Choisir un restaurant"}
+        </span>
+      </div>
+
       <MdArrowForwardIos className="h-5 w-5 shrink-0 rotate-90" />
     </div>
   );
--- apps/web/components/ui/button/text.tsx
+++ apps/web/components/ui/button/text.tsx
@@ -4,7 +4,7 @@ export const TextButton = ({ children, className, ...props }: { children: React.
   return (
     <button
       className={cn(
-        `inline-flex w-full items-center justify-center gap-x-2 text-sm font-bold uppercase duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2`,
+        `inline-flex w-full items-center focus:appearance-none justify-center gap-x-2 text-sm font-bold uppercase duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2`,
         className,
       )}
       {...props}
