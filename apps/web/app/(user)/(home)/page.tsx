302ab7c486f1c699ccf909d920d5fb7df83a83c3
Florian LEROUX <florian.leroux3@laposte.net>
2023-09-26 14:55:27 +0200

    feat(auth/menu): add menu links, not auth pages and components

--- apps/web/app/(user)/(home)/page.tsx
+++ apps/web/app/(user)/(home)/page.tsx
@@ -1,14 +1,12 @@
 import { Catalog } from "@/components/catalog";
 import { RatingSection } from "@/app/(user)/(home)/rating";
 import { BasketWrapper } from "@/components/basket";
-import { Navbar } from "@/components/ui/navbar";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <RatingSection />
-      <div className="has-background-grid relative p-2 pt-6">
+      <div className="has-background-grid grow h-full relative p-2 py-6">
         <main className="sticky mx-auto grid max-w-7xl gap-2 border border-gray-100 bg-white p-2 shadow-xl lg:grid-cols-3">
           <Catalog />
           <div className="hidden lg:block">
---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { NotFound } from "@/components/ui/not-found";
+
+export default function RootNotFound() {
+  return <NotFound className="h-screen" />;
+}
