78a444cb30e4411425883d1d0f4658ca5d25e4b9
Florian LEROUX <florian.leroux3@laposte.net>
2023-09-26 14:58:25 +0200

    ref(location): change location behavior

--- apps/web/components/location/sheet-content.tsx
+++ apps/web/components/location/sheet-content.tsx
@@ -4,12 +4,19 @@ import { LocationRestaurant } from "@/components/location/restaurant";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { restaurantList } from "@/constants/data";
-import { useLocation } from "@/hooks/useLocation";
+import { useBasket, useLocation } from "@/hooks";
 import { useEffect, useState } from "react";
 import { MdLocationOn } from "react-icons/md";
 
 export const LocationSheetContent = ({ closeModal = () => {} }) => {
   const { lat, lng, refreshLocation } = useLocation();
+  const { address, setAddress } = useBasket();
+  const { street, zipcode, city, country } = address || {
+    street: "",
+    zipcode: "",
+    city: "",
+    country: "France",
+  };
 
   // todo: might need to store restaurants in context to avoid re-fetching and have data
   const [restaurants, setRestaurants] = useState(restaurantList);
@@ -32,6 +39,41 @@ export const LocationSheetContent = ({ closeModal = () => {} }) => {
 
   return (
     <div className="flex w-full flex-col gap-y-4">
+      <div className="flex items-center justify-between text-sm font-bold">Adresse de livraison</div>
+      <div className="grid grid-cols-8 gap-2 w-full">
+        <Input
+          type="text"
+          aria-label="Rue"
+          placeholder="Rue"
+          value={street}
+          onChange={(e) => setAddress({ zipcode, city, country, street: e.target.value })}
+          wrapperClassName="col-span-8"
+        />
+        <Input
+          type="text"
+          aria-label="Code postal"
+          placeholder="Code postal"
+          value={zipcode}
+          onChange={(e) => setAddress({ zipcode: e.target.value, city, country, street })}
+          wrapperClassName="col-span-2"
+        />
+        <Input
+          type="text"
+          aria-label="Ville"
+          placeholder="Ville"
+          value={city}
+          onChange={(e) => setAddress({ zipcode, city: e.target.value, country, street })}
+          wrapperClassName="col-span-4"
+        />
+        <Input
+          type="text"
+          aria-label="Pays"
+          placeholder="Pays"
+          value={country}
+          onChange={(e) => setAddress({ zipcode, city, country: e.target.value, street })}
+          wrapperClassName="col-span-2"
+        />
+      </div>
       <div className="flex items-center justify-between text-sm font-bold">Choisir un restaurant</div>
       <Input
         type="search"
