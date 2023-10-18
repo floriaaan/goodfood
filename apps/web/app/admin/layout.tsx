6b7d9d3dd8cb839a3937f712c8c02b690e25db43
Utilisateur <anatole.godard@icloud.com>
2023-10-18 16:21:33 +0200

    feat(admin): Style the sidebar

--- apps/web/app/admin/layout.tsx
+++ apps/web/app/admin/layout.tsx
@@ -1,8 +1,11 @@
 import { Sidebar } from "@/components/ui/admin/sidebar";
+import { ReactNode } from "react";
 
-export default function UserLayout({ children }: { children: React.ReactNode }) {
-  return <>
-    <Sidebar />
-    <div>{children} </div>
-    </>
-  }
+export default function AdminLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="inline-flex w-full overflow-hidden">
+      <Sidebar />
+      <div className="max-h-screen grow overflow-y-auto">{children}</div>
+    </div>
+  );
+}
