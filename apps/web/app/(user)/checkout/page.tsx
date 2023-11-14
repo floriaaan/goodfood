0145ec92056192a2d1aab53edc851b44dab71e11
Florian LEROUX <florian.leroux3@laposte.net>
2023-11-14 13:09:13 +0100

    feat(checkout): add safeguard for page

--- apps/web/app/(user)/checkout/page.tsx
+++ apps/web/app/(user)/checkout/page.tsx
@@ -8,7 +8,7 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { cn } from "@/lib/utils";
 import { DeliveryType, Order } from "@/types/order";
 import { useState } from "react";
-import { MdCheck, MdLock, MdShoppingBasket } from "react-icons/md";
+import { MdCheck, MdLock, MdRestaurant, MdShoppingBasket } from "react-icons/md";
 
 import { loadStripe } from "@stripe/stripe-js";
 import { EmbeddedCheckoutProvider, EmbeddedCheckout } from "@stripe/react-stripe-js";
@@ -19,10 +19,15 @@ import { orderList, restaurantList } from "@/constants/data";
 import { TiLocationArrow } from "react-icons/ti";
 
 import { Player } from "@lottiefiles/react-lottie-player";
+import { useBasket } from "@/hooks";
+import Link from "next/link";
+import { LocationFullTrigger } from "@/components/location";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
 
 export default function CheckoutPage() {
+  const { isAuthenticated, isBasketEmpty, isRestaurantSelected } = useBasket();
+
   const [deliveryType, setDeliveryType] = useState(DeliveryType.DELIVERY);
 
   const [delivery_isModalOpen, setDelivery_isModalOpen] = useState(false);
@@ -34,141 +39,173 @@ export default function CheckoutPage() {
 
   return (
     <div className="flex h-full grow p-4 pb-12">
-      <div className="mx-auto flex h-fit w-full max-w-6xl flex-col-reverse gap-2 lg:grid lg:grid-cols-3">
-        <main className="sticky col-span-2  flex w-full flex-col gap-2 border border-gray-100 bg-white p-4">
-          <h2 className="text-xl font-semibold">Paiement</h2>
-          <small>Sélectionnez votre mode de paiement</small>
-          <RadioGroup
-            onValueChange={(e) => {
-              setDeliveryType(e as DeliveryType);
-            }}
-            defaultValue={DeliveryType.DELIVERY}
-          >
-            <Label
-              htmlFor={DeliveryType.TAKEAWAY}
-              className={cn(
-                "h-full w-full cursor-pointer leading-6",
-                "flex cursor-pointer items-center gap-x-2 border p-4",
-                deliveryType !== DeliveryType.TAKEAWAY && "opacity-50",
-              )}
+      {isAuthenticated && isRestaurantSelected && !isBasketEmpty && (
+        <div className="mx-auto flex h-fit w-full max-w-6xl flex-col-reverse gap-2 lg:grid lg:grid-cols-3">
+          <main className="sticky col-span-2  flex w-full flex-col gap-2 border border-gray-100 bg-white p-4">
+            <h2 className="text-xl font-semibold">Paiement</h2>
+            <small>Sélectionnez votre mode de paiement</small>
+            <RadioGroup
+              onValueChange={(e) => {
+                setDeliveryType(e as DeliveryType);
+              }}
+              defaultValue={DeliveryType.DELIVERY}
             >
-              <RadioGroupItem value={DeliveryType.TAKEAWAY} id={DeliveryType.TAKEAWAY} />
-              {"Je paye sur place et j’emporte ma commande"}
-            </Label>
-            <Label
-              htmlFor={DeliveryType.DELIVERY}
-              className={cn(
-                "w-full cursor-pointer leading-6",
-                "flex cursor-pointer items-center gap-x-2 border p-4",
-                deliveryType !== DeliveryType.DELIVERY && "opacity-50",
-              )}
-            >
-              <RadioGroupItem value={DeliveryType.DELIVERY} id={DeliveryType.DELIVERY} />
-              Je fais livrer ma commande et je paye maintenant par carte bancaire
-            </Label>
-          </RadioGroup>
+              <Label
+                htmlFor={DeliveryType.TAKEAWAY}
+                className={cn(
+                  "h-full w-full cursor-pointer leading-6",
+                  "flex cursor-pointer items-center gap-x-2 border p-4",
+                  deliveryType !== DeliveryType.TAKEAWAY && "opacity-50",
+                )}
+              >
+                <RadioGroupItem value={DeliveryType.TAKEAWAY} id={DeliveryType.TAKEAWAY} />
+                {"Je paye sur place et j’emporte ma commande"}
+              </Label>
+              <Label
+                htmlFor={DeliveryType.DELIVERY}
+                className={cn(
+                  "w-full cursor-pointer leading-6",
+                  "flex cursor-pointer items-center gap-x-2 border p-4",
+                  deliveryType !== DeliveryType.DELIVERY && "opacity-50",
+                )}
+              >
+                <RadioGroupItem value={DeliveryType.DELIVERY} id={DeliveryType.DELIVERY} />
+                Je fais livrer ma commande et je paye maintenant par carte bancaire
+              </Label>
+            </RadioGroup>
 
-          <div className="w-full">
-            {deliveryType === DeliveryType.DELIVERY && (
-              <>
-                {/* DELIVERY */}
-                <Button
-                  className="h-16 w-full"
-                  onClick={() => {
-                    // TODO: stripe
-                    setDelivery_isModalOpen(true);
-                  }}
-                >
-                  <MdShoppingBasket className="h-4 w-4 shrink-0" />
-                  Payer
-                </Button>
-                <Dialog onOpenChange={setDelivery_isModalOpen} open={delivery_isModalOpen}>
-                  <DialogContent className="flex aspect-video h-auto max-w-4xl items-center justify-center">
-                    <div id="checkout">
-                      {delivery_checkoutSessionSecret ? (
-                        <EmbeddedCheckoutProvider
-                          stripe={stripePromise}
-                          options={{ clientSecret: delivery_checkoutSessionSecret }}
-                        >
-                          <EmbeddedCheckout />
-                        </EmbeddedCheckoutProvider>
-                      ) : (
-                        <div className="h-64 w-64">
-                          <LargeComponentLoader />
-                        </div>
-                      )}
-                    </div>
-                  </DialogContent>
-                </Dialog>
-              </>
-            )}
-            {deliveryType === DeliveryType.TAKEAWAY && (
-              <>
-                {/* TAKEAWAY */}
-                <Button
-                  className="h-16 w-full"
-                  onClick={() => {
-                    // TODO: create order
-                    setTakeaway_isModalOpen(true);
-                  }}
-                >
-                  <MdCheck className="h-4 w-4 shrink-0" />
-                  Finaliser et payer sur place
-                </Button>
-                <Dialog onOpenChange={setTakeaway_isModalOpen} open={takeaway_isModalOpen}>
-                  <DialogContent className="grid aspect-video h-auto max-w-4xl grid-cols-2 gap-2 ">
-                    {order &&
-                      (() => {
-                        const restaurant = restaurantList.find((r) => r.id === order.restaurant_id);
-                        if (!restaurant) return null;
-                        return (
-                          <>
-                            <CheckoutReceipt {...order} />
-                            <div className="mt-4 flex flex-col items-center gap-y-4 text-center">
-                              <div className="">
-                                <h2 className="text-xl font-semibold">Votre commande est en attente</h2>
-                                <p className="text-sm">Vous pouvez venir la chercher dès maintenant au restaurant.</p>
+            <div className="w-full">
+              {deliveryType === DeliveryType.DELIVERY && (
+                <>
+                  {/* DELIVERY */}
+                  <Button
+                    className="h-16 w-full"
+                    onClick={() => {
+                      // TODO: stripe
+                      setDelivery_isModalOpen(true);
+                    }}
+                  >
+                    <MdShoppingBasket className="h-4 w-4 shrink-0" />
+                    Payer
+                  </Button>
+                  <Dialog onOpenChange={setDelivery_isModalOpen} open={delivery_isModalOpen}>
+                    <DialogContent className="flex aspect-video h-auto max-w-4xl items-center justify-center">
+                      <div id="checkout">
+                        {delivery_checkoutSessionSecret ? (
+                          <EmbeddedCheckoutProvider
+                            stripe={stripePromise}
+                            options={{ clientSecret: delivery_checkoutSessionSecret }}
+                          >
+                            <EmbeddedCheckout />
+                          </EmbeddedCheckoutProvider>
+                        ) : (
+                          <div className="h-64 w-64">
+                            <LargeComponentLoader />
+                          </div>
+                        )}
+                      </div>
+                    </DialogContent>
+                  </Dialog>
+                </>
+              )}
+              {deliveryType === DeliveryType.TAKEAWAY && (
+                <>
+                  {/* TAKEAWAY */}
+                  <Button
+                    className="h-16 w-full"
+                    onClick={() => {
+                      // TODO: create order
+                      setTakeaway_isModalOpen(true);
+                    }}
+                  >
+                    <MdCheck className="h-4 w-4 shrink-0" />
+                    Finaliser et payer sur place
+                  </Button>
+                  <Dialog onOpenChange={setTakeaway_isModalOpen} open={takeaway_isModalOpen}>
+                    <DialogContent className="grid aspect-video h-auto max-w-4xl grid-cols-2 gap-2 ">
+                      {order &&
+                        (() => {
+                          const restaurant = restaurantList.find((r) => r.id === order.restaurant_id);
+                          if (!restaurant) return null;
+                          return (
+                            <>
+                              <CheckoutReceipt {...order} />
+                              <div className="mt-4 flex flex-col items-center gap-y-4 text-center">
+                                <div className="">
+                                  <h2 className="text-xl font-semibold">Votre commande est en attente</h2>
+                                  <p className="text-sm">Vous pouvez venir la chercher dès maintenant au restaurant.</p>
+                                </div>
+                                <div className="grow">
+                                  <Player
+                                    autoplay
+                                    src={"https://lottie.host/a4822d55-540f-4efa-8d47-b87b633e1e64/qDllUOhZG4.json"}
+                                    loop
+                                    speed={1}
+                                  />
+                                </div>
+                                <Button asChild>
+                                  <a
+                                    href={`https://www.google.com/maps?q=${restaurant.locationList[0]},${restaurant.locationList[1]}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                  >
+                                    <TiLocationArrow className="-mt-0.5 h-6 w-6 shrink-0" />
+                                    {"J'y vais"}
+                                  </a>
+                                </Button>
                               </div>
-                              <div className="grow">
-                                <Player
-                                  autoplay
-                                  src={"https://lottie.host/a4822d55-540f-4efa-8d47-b87b633e1e64/qDllUOhZG4.json"}
-                                  loop
-                                  speed={1}
-                                />
-                              </div>
-                              <Button asChild>
-                                <a
-                                  href={`https://www.google.com/maps?q=${restaurant.locationList[0]},${restaurant.locationList[1]}`}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  <TiLocationArrow className="-mt-0.5 h-6 w-6 shrink-0" />
-                                  {"J'y vais"}
-                                </a>
-                              </Button>
-                            </div>
-                          </>
-                        );
-                      })()}
-                  </DialogContent>
-                </Dialog>
-              </>
-            )}
+                            </>
+                          );
+                        })()}
+                    </DialogContent>
+                  </Dialog>
+                </>
+              )}
+            </div>
+          </main>
+          <div className="flex flex-col gap-1">
+            <CheckoutRecap />
+            <small className="mt-2 inline-flex flex-wrap items-center justify-center gap-1 text-center text-[10px] leading-3 text-gray-500">
+              En passant votre commande, vous acceptez nos
+              <u className="font-semibold">Conditions générales de vente</u>
+            </small>
+            <small className="mt-2 inline-flex flex-wrap items-center justify-center gap-1 text-center text-[10px] leading-3 text-gray-500">
+              <MdLock className="h-4 w-4 shrink-0" />
+              Paiement 100% sécurisé par <u className="font-semibold">Stripe</u>
+            </small>
           </div>
-        </main>
-        <div className="flex flex-col gap-1">
-          <CheckoutRecap />
-          <small className="mt-2 inline-flex flex-wrap items-center justify-center gap-1 text-center text-[10px] leading-3 text-gray-500">
-            En passant votre commande, vous acceptez nos
-            <u className="font-semibold">Conditions générales de vente</u>
-          </small>
-          <small className="mt-2 inline-flex flex-wrap items-center justify-center gap-1 text-center text-[10px] leading-3 text-gray-500">
-            <MdLock className="h-4 w-4 shrink-0" />
-            Paiement 100% sécurisé par <u className="font-semibold">Stripe</u>
-          </small>
         </div>
-      </div>
+      )}
+      {!isAuthenticated && (
+        <div className="flex w-full flex-col items-center justify-center gap-2">
+          <MdLock className="h-8 w-8" />
+          <span className="text-xl font-semibold">Vous devez être connecté pour passer commande</span>
+          <small className="text-sm">Vous pouvez le faire dans la barre de navigation</small>
+          <hr className="border-gray-300" />
+
+          <Button asChild className="w-64">
+            <Link href="/login">Se connecter</Link>
+          </Button>
+        </div>
+      )}
+      {isAuthenticated && !isRestaurantSelected && (
+        <div className="flex w-full flex-col items-center justify-center gap-2">
+          <MdRestaurant className="h-8 w-8" />
+          <span className="text-xl font-semibold">Vous devez sélectionner un restaurant</span>
+          <small className="text-sm">Vous pouvez le faire dans la barre de navigation</small>
+        </div>
+      )}
+      {isAuthenticated && isRestaurantSelected && isBasketEmpty && (
+        <div className="flex w-full flex-col items-center justify-center gap-2">
+          <MdShoppingBasket className="h-8 w-8" />
+          <span className="text-xl font-semibold">Votre panier doit contenir au moins un produit</span>
+          <small className="text-sm">Vous pouvez le faire sur la page de catalogue</small>
+          <hr className="border-gray-300" />
+          <Button asChild className="w-64">
+            <Link href="/">Voir le catalogue</Link>
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
