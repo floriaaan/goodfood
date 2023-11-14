22487ac8cbe5cea6e7b2c536660f0acfede1d61e
Florian LEROUX <florian.leroux3@laposte.net>
2023-11-14 16:00:50 +0100

    ref(catalog): add no product

--- apps/web/app/(user)/(home)/catalog/filters.tsx
+++ apps/web/app/(user)/(home)/catalog/filters.tsx
@@ -1,4 +1,4 @@
-import { useCatalogFilters } from "@/components/catalog";
+import { useCatalogFilters } from "@/app/(user)/(home)/catalog";
 import { cn } from "@/lib/utils";
 import { ProductType } from "@/types/product";
 
