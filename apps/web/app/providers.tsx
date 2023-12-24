61c76f9f723ef5e7ec19f2dcee9af9ddce570e6a
Florian LEROUX <florian.leroux3@laposte.net>
2023-12-24 18:56:43 +0100

    feat(query): add tanstack/query to fetch datas using custom fetcher

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,13 +1,30 @@
 "use client";
 import { AuthProvider } from "@/hooks";
 import { LocationProvider } from "@/hooks/useLocation";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const RootProviders = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
   return (
     <>
-      <LocationProvider>
-        <AuthProvider>{children}</AuthProvider>
-      </LocationProvider>
+      <QueryClientProvider client={queryClient}>
+        <LocationProvider>
+          <AuthProvider>{children}</AuthProvider>
+        </LocationProvider>
+      </QueryClientProvider>
     </>
   );
 };
