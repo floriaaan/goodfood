f220a8d50679b7f21900b4760d19c589750037f7
Florian LEROUX <florian.leroux3@laposte.net>
2024-01-08 15:06:28 +0100

    feat(link): add login into provider

--- apps/web/app/auth/login/page.tsx
+++ apps/web/app/auth/login/page.tsx
@@ -1,10 +1,12 @@
+import { LoginForm } from "@/app/auth/login/form";
 import Link from "next/link";
 
 export default function Login() {
   return (
-    <>
+    <div className="flex flex-col gap-4 p-4">
       <h1>Login</h1>
+      <LoginForm />
       <Link href="/auth/register">Register</Link>
-    </>
+    </div>
   );
 }
--- apps/web/hooks/useAuth.tsx
+++ apps/web/hooks/useAuth.tsx
@@ -1,24 +1,41 @@
 "use client";
+import { setCookie, getCookie } from "cookies-next";
 
 /**
- * TODO: Replace this with a real authentication system
  * TODO: add default restaurant from mainAddress (the nearest one)
  */
 
 import { User } from "@/types/user";
 import { useState, createContext, useContext } from "react";
 
-import { user as user_tmp } from "@/constants/data";
+// import { user as user_tmp } from "@/constants/data";
+import { fetchAPI } from "@/lib/fetchAPI";
+
+export type Session =
+  | {
+      user: User;
+      token: string;
+      error: null;
+    }
+  | {
+      user: null;
+      token: null;
+      error: string;
+    };
 
 type AuthContextType = {
+  session: Session | null;
   user: User | null;
-  login: (email: string, password: string) => void;
+  isAuthentified: boolean;
+  login: (email: string, password: string) => Promise<boolean>;
   logout: () => void;
 };
 
 const AuthContext = createContext<AuthContextType>({
+  session: null,
   user: null,
-  login: () => {},
+  isAuthentified: false,
+  login: (() => {}) as unknown as AuthContextType["login"],
   logout: () => {},
 });
 
@@ -29,19 +46,46 @@ export const useAuth = () => {
 };
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState<User | null>(
-    // null,
-    user_tmp,
+  const [session, setSession] = useState<Session | null>(
+    (() => {
+      const token = getCookie("gf-token");
+      const user = getCookie("gf-user");
+      if (token && user) return { token, user: JSON.parse(user), error: null };
+      return null;
+    })(),
   );
 
-  const login = (email: string, password: string) => {
-    // TODO: replace this with a real authentication system
-    setUser(user_tmp);
+  const login = async (email: string, password: string) => {
+    const res = await fetchAPI("/api/user/login", undefined, {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+    });
+    const body = await res.json();
+    if (!res.ok || res.status !== 200) throw body;
+
+    setSession(body);
+    setCookie("gf-token", body.token, {
+      // make max age last time defined in user service
+      maxAge: 30 * 24 * 60 * 60,
+    });
+    setCookie("gf-user", JSON.stringify(body.user), {
+      // make max age last time defined in user service
+      maxAge: 30 * 24 * 60 * 60,
+    });
+    return true;
   };
 
   const logout = () => {
-    setUser(null);
+    setSession(null);
+    setCookie("gf-token", null, { maxAge: -1 });
+    setCookie("gf-user", null, { maxAge: -1 });
   };
 
-  return <AuthContext.Provider value={{ user, login, logout }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider
+      value={{ session, login, logout, user: session?.user ?? null, isAuthentified: session?.user !== null }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
