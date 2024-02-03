bdb681c7047a85cc025ea46b1bebfcf8627695a4
Florian LEROUX <florian.leroux3@laposte.net>
2024-02-04 00:25:50 +0100

    feat(account): add promos page

--- apps/web/app/(normal)/account/layout.tsx
+++ apps/web/app/(normal)/account/layout.tsx
@@ -10,7 +10,7 @@ export default function UserAccountLayout({ children }: { children: React.ReactN
     <>
       <div className="mx-auto flex h-full w-full grow flex-col gap-2 px-2 pt-2 lg:max-w-7xl lg:flex-row lg:gap-8 lg:px-8 lg:pt-4">
         <Sidebar />
-        <div className="flex w-full grow flex-col gap-3 overflow-x-auto pb-6 pt-4">{children}</div>
+        <div className="flex w-full grow flex-col gap-3 overflow-x-auto pb-6 ">{children}</div>
       </div>
     </>
   );
