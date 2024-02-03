e3b8fa2d45b5b3aa759e432f8198c25dbdcb4b71
Florian LEROUX <florian.leroux3@laposte.net>
2024-02-03 16:51:44 +0100

    feat(account): add change password form

--- apps/web/app/(normal)/account/layout.tsx
+++ apps/web/app/(normal)/account/layout.tsx
@@ -1,10 +1,17 @@
+"use client";
 import { Sidebar } from "@/components/ui/user/sidebar";
+import { useAuth } from "@/hooks";
 
 export default function UserAccountLayout({ children }: { children: React.ReactNode }) {
+  const { user } = useAuth();
+  if (!user) return "Not authenticated";
+
   return (
-    <div className="flex h-full grow flex-col lg:flex-row">
-      <Sidebar />
-      <div className="flex w-full grow flex-col gap-3 px-6 pb-6 pt-4">{children}</div>
-    </div>
+    <>
+      <div className="mx-auto flex h-full w-full grow flex-col gap-2 px-2 pt-2 lg:max-w-7xl lg:flex-row lg:gap-8 lg:px-8 lg:pt-4">
+        <Sidebar />
+        <div className="flex w-full grow flex-col gap-3 overflow-x-auto pb-6 pt-4">{children}</div>
+      </div>
+    </>
   );
 }
