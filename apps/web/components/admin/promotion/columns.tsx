d5fc494097539967453125cd4a1e761c2fb5b9a5
Florian LEROUX <florian.leroux3@laposte.net>
2024-02-04 15:39:30 +0100

    feat(promotion): add update & delete, linked with gateway

--- apps/web/components/admin/promotion/columns.tsx
+++ apps/web/components/admin/promotion/columns.tsx
@@ -1,21 +1,10 @@
 "use client";
 
-import { PromotionCreateEditFormValues } from "@/components/admin/promotion/form";
-import { PromotionFormSheetContent } from "@/components/admin/promotion/sheet-content";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+import { PromotionActions } from "@/components/admin/promotion/actions";
 import { cn } from "@/lib/utils";
 import { Promotion } from "@/types/promotion";
 import { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
-import { MdArrowDropUp, MdCopyAll, MdEdit } from "react-icons/md";
+import { MdArrowDropUp } from "react-icons/md";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -69,34 +58,7 @@ export const promotions_columns: ColumnDef<Promotion>[] = [
     cell: ({ row }) => {
       const p = row.original;
 
-      return (
-        <Sheet>
-          <DropdownMenu modal={false}>
-            <DropdownMenuTrigger>
-              <>
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-              </>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="flex flex-col gap-y-1 p-2">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem onClick={() => navigator.clipboard.writeText(p.id)}>
-                <MdCopyAll className="h-4 w-4 shrink-0" />
-                {"Copier le code promo"}
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-
-              <DropdownMenuItem>
-                <SheetTrigger className="inline-flex items-center gap-x-1">
-                  <MdEdit className="h-4 w-4 shrink-0" />
-                  Modifier/supprimer
-                </SheetTrigger>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-          <PromotionFormSheetContent initialValues={{ ...p } as unknown as PromotionCreateEditFormValues} id={p.id} />
-        </Sheet>
-      );
+      return <PromotionActions {...p} />;
     },
   },
 ];
--- apps/web/components/admin/promotion/sheet-content.tsx
+++ apps/web/components/admin/promotion/sheet-content.tsx
@@ -12,76 +12,73 @@ import { MdDone } from "react-icons/md";
 export const PromotionFormSheetContent = ({
   initialValues,
   id,
+  closeSheet,
 }: {
   initialValues?: PromotionCreateEditFormValues;
   id?: Promotion["id"];
+  closeSheet: () => void;
 }) => {
   const { session } = useAuth();
   const { refetchPromotions } = useAdmin();
   if (!session) return null;
-  const onSubmit = async (values: PromotionCreateEditFormValues) => {
-    // eslint-disable-next-line no-console
-    if (id) {
-      const res = await fetchAPI(`/api/promotion/${id}`, session.token, {
-        method: "PUT",
-        body: JSON.stringify(values),
-      });
-      refetchPromotions();
-      if (!res.ok)
-        return toast({
-          className: "p-3",
-          children: (
-            <div className="inline-flex w-full items-end justify-between gap-2">
-              <div className="inline-flex shrink-0 gap-2">
-                <XIcon className="h-6 w-6 text-red-500" />
-                <div className="flex w-full grow flex-col">
-                  <ToastTitle>Erreur lors de la mise à jour de la promotion</ToastTitle>
-                </div>
-              </div>
-            </div>
-          ),
-        });
-      return toast({
-        className: "p-3",
-        children: (
-          <div className="inline-flex w-full items-end justify-between gap-2">
-            <div className="inline-flex shrink-0 gap-2">
-              <MdDone className="h-6 w-6 text-green-500" />
-              <div className="flex w-full grow flex-col">
-                <ToastTitle>La promotion a été mise à jour avec succès</ToastTitle>
-              </div>
+
+  const update = async (values: PromotionCreateEditFormValues) => {
+    const res = await fetchAPI(`/api/promotion/${id}`, session.token, {
+      method: "PUT",
+      body: JSON.stringify(values),
+    });
+    if (!res.ok) throw new Error("Une erreur s'est produite lors de la mise à jour de la promotion");
+    return toast({
+      className: "p-3",
+      children: (
+        <div className="inline-flex w-full items-end justify-between gap-2">
+          <div className="inline-flex shrink-0 gap-2">
+            <MdDone className="h-6 w-6 text-green-500" />
+            <div className="flex w-full grow flex-col">
+              <ToastTitle>La promotion a été mise à jour avec succès</ToastTitle>
             </div>
           </div>
-        ),
-      });
-    } else {
-      const res = await fetchAPI("/api/promotion", session.token, {
-        method: "POST",
-        body: JSON.stringify(values),
-      });
-      refetchPromotions();
-      if (!res.ok)
-        return toast({
-          className: "p-3",
-          children: (
-            <div className="inline-flex w-full items-end justify-between gap-2">
-              <div className="inline-flex shrink-0 gap-2">
-                <XIcon className="h-6 w-6 text-red-500" />
-                <div className="flex w-full grow flex-col">
-                  <ToastTitle>Erreur lors de la création de la promotion</ToastTitle>
-                </div>
-              </div>
+        </div>
+      ),
+    });
+  };
+
+  const create = async (values: PromotionCreateEditFormValues) => {
+    const res = await fetchAPI("/api/promotion", session.token, {
+      method: "POST",
+      body: JSON.stringify(values),
+    });
+    if (!res.ok) throw new Error("Une erreur s'est produite lors de la création de la promotion");
+    return toast({
+      className: "p-3",
+      children: (
+        <div className="inline-flex w-full items-end justify-between gap-2">
+          <div className="inline-flex shrink-0 gap-2">
+            <MdDone className="h-6 w-6 text-green-500" />
+            <div className="flex w-full grow flex-col">
+              <ToastTitle>La promotion a été créée avec succès</ToastTitle>
             </div>
-          ),
-        });
-      return toast({
+          </div>
+        </div>
+      ),
+    });
+  };
+
+  const onSubmit = async (values: PromotionCreateEditFormValues) => {
+    try {
+      if (id) update(values);
+      else create(values);
+      refetchPromotions();
+      closeSheet();
+    } catch (err) {
+      toast({
         className: "p-3",
         children: (
           <div className="inline-flex w-full items-end justify-between gap-2">
             <div className="inline-flex shrink-0 gap-2">
-              <MdDone className="h-6 w-6 text-green-500" />
+              <XIcon className="h-6 w-6 text-red-500" />
               <div className="flex w-full grow flex-col">
-                <ToastTitle>La promotion a été créée avec succès</ToastTitle>
+                <ToastTitle>{(err as Error).message ?? "Une erreur est survenue."}</ToastTitle>
               </div>
             </div>
           </div>
@@ -93,7 +90,7 @@ export const PromotionFormSheetContent = ({
   return (
     <SheetContent side="right" className="w-full border-l-0 sm:max-w-xl">
       <div className="flex h-full w-full">
-        <PromotionCreateEditForm {...{ onSubmit, initialValues, id }} />
+        <PromotionCreateEditForm {...{ onSubmit, initialValues, id, closeSheet }} />
       </div>
       <SheetClose />
     </SheetContent>
