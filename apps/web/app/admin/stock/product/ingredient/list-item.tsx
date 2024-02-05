a2af8ca695b819f6d9bf51142139ba904e2b52ed
Florian LEROUX <florian.leroux3@laposte.net>
2024-02-06 00:50:00 +0100

    feat(stock) [wip]: add stock page

---
+++ apps/web/app/admin/stock/product/ingredient/list-item.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { IngredientRestaurantSheetContent } from "@/app/admin/stock/product/ingredient/sheet-content";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+import { IngredientRestaurant } from "@/types/stock";
+import { useState } from "react";
+
+export const IngredientRestaurantListItem = (ir: IngredientRestaurant) => {
+  const isThresholdReached = ir.quantity <= ir.alertThreshold;
+  const isOutOfStock = ir.quantity <= 0;
+  const [open, onOpenChange] = useState(false);
+
+  return (
+    <Sheet {...{ open, onOpenChange }}>
+      <SheetTrigger asChild>
+        <div className="relative flex cursor-pointer flex-col gap-x-2 gap-y-1 bg-neutral-100 px-2 py-1 text-xs">
+          <strong className="text-sm first-letter:uppercase">{ir.ingredient.name}</strong>
+          <div className="flex gap-2">
+            <span className="min-w-[64px] shrink-0">Quantité</span> <strong>{ir.quantity}</strong>
+          </div>
+          <div className="flex gap-2">
+            <span className="min-w-[64px] shrink-0">Seuil</span> <strong>{ir.alertThreshold}</strong>
+          </div>
+          <div className="absolute right-1 top-1 text-[10px]">
+            {isThresholdReached && !isOutOfStock && (
+              <div className="flex items-center gap-2 text-amber-700">
+                <span>{"Seuil d'alerte atteint"}</span>
+              </div>
+            )}
+            {isOutOfStock && (
+              <div className="flex items-center gap-2 text-red-700">
+                <span>{"Rupture de stock"}</span>
+              </div>
+            )}
+          </div>
+        </div>
+      </SheetTrigger>
+      <IngredientRestaurantSheetContent closeSheet={() => onOpenChange(false)} ingredientRestaurant={ir} />
+    </Sheet>
+  );
+};
