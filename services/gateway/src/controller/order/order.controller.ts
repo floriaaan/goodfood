e7a7e76c7e0926566395edc3b8181ccd06943fe3
Anatole-Godard <anatole.godard@icloud.com>
2024-02-07 18:25:39 +0100

    [FEATURE]: add payment in order response from gateway

--- services/gateway/src/controller/order/order.controller.ts
+++ services/gateway/src/controller/order/order.controller.ts
@@ -1,5 +1,4 @@
 import { check, withCheck } from "@gateway/middleware/auth";
-import { Address, Delivery } from "@gateway/proto/delivery_pb";
 import {
   Basket as BasketSnapshot,
   CreateOrderRequest,
@@ -7,25 +6,22 @@ import {
   GetOrderByDeliveryRequest,
   GetOrderByPaymentRequest,
   GetOrderRequest,
+  GetOrdersByRestaurantRequest,
   GetOrdersByStatusRequest,
   GetOrdersByUserRequest,
   Order,
   Status,
   UpdateOrderRequest,
   UserMinimum,
-  GetOrdersByRestaurantRequest,
 } from "@gateway/proto/order_pb";
 import { User } from "@gateway/proto/user_pb";
-import { getBasketByUser, resetBasketByUser } from "@gateway/services/basket.service";
-import { createDelivery } from "@gateway/services/delivery.service";
-import { Address, Delivery } from "@gateway/proto/delivery_pb";
-import { getRestaurant } from "@gateway/services/restaurant.service";
+import { getBasketByUser } from "@gateway/services/basket.service";
 import { getUser, getUserIdFromToken } from "@gateway/services/user.service";
 import { Request, Response, Router } from "express";
 import orderService from "../../services/clients/order.client";
-import { getRestaurant } from "@gateway/services/restaurant.service";
 import { restaurantServiceClient } from "@gateway/services/clients";
 import { Restaurant, RestaurantId } from "@gateway/proto/restaurant_pb";
+import { getPayment } from "@gateway/services/payment.service";
 
 export const orderRoutes = Router();
 
@@ -64,8 +60,8 @@ orderRoutes.get("/api/order/:id", async (req: Request, res: Response) => {
     if (!order) return res.status(404).send({ error: "Order not found" });
     if (order.user?.id !== userId.toString() || !(await check(token, { role: "ADMIN" })))
       return res.status(401).send({ error: "Unauthorized" });
-
-    return res.status(200).json(order);
+    const payment = await getPayment(order.paymentId);
+    return res.status(200).json({ ...order, payment: payment?.toObject() });
   } catch (error) {
     return res.status(500).send({ error });
   }
@@ -129,101 +125,12 @@ orderRoutes.post("/api/order", async (req: Request, res: Response) => {
     .setBasketSnapshot(orderBasket)
     .setRestaurantId(restaurantId);
 
-  orderService.createOrder(orderInput, (error, response) => {
+  orderService.createOrder(orderInput, async (error, response) => {
+    const order = response.toObject();
+    const payment = await getPayment(order.paymentId);
     if (error) return res.status(500).send({ error });
-    else return res.status(200).json(response.toObject());
+    else return res.status(200).json({ ...order, payment: payment?.toObject() });
   });
-});
-
-//TODO: to test this route with the user and basket service
-orderRoutes.post("/api/order/tmp", async (req: Request, res: Response) => {
-  /* #swagger.parameters['body'] = {
-            in: 'body',
-            required: true,
-            schema: {
-                paymentId: "payment_id:1",
-                deliveryType :{'$ref': '#/definitions/DeliveryType'},
-            }
-      }
-      #swagger.parameters['authorization'] = {
-        in: 'header',
-        required: true,
-        type: 'string'
-    } */
-
-  // Auth check and :id check ---
-  const { authorization } = req.headers;
-  if (!authorization) return res.status(401).json({ message: "Unauthorized" });
-  const token = authorization.split("Bearer ")[1];
-  const userId = await getUserIdFromToken(token);
-  if (!userId) return res.status(401).json({ message: "Unauthorized" });
-  // ----------------------------
-
-  const { paymentId, deliveryType } = req.body;
-  let user: User | undefined = undefined;
-  try {
-    user = await getUser(userId);
-  } catch (error) {
-    return res.status(500).send({ error });
-  }
-
-  if (!user) return res.status(404).send({ error: "User not found" });
-  const userToObj = user.toObject();
-  const miniUser = new UserMinimum()
-    .setId(String(userToObj.id))
-    .setEmail(userToObj.email)
-    .setFirstName(userToObj.firstName)
-    .setLastName(userToObj.lastName)
-    .setPhone(userToObj.phone);
-
-  let orderBasket: BasketSnapshot | undefined = undefined;
-  let restaurantId = "";
-  try {
-    const basket = (await getBasketByUser(userId.toString()))?.toObject();
-    if (basket) {
-      orderBasket = new BasketSnapshot().setString(JSON.stringify(basket));
-      restaurantId = basket.restaurantId;
-    } else {
-      return res.status(404).send({ error: "Basket not found" });
-    }
-  } catch (error) {
-    return res.status(500).send({ error });
-  }
-
-  let delivery: Delivery | undefined = undefined;
-  try {
-    const restaurant = await getRestaurant(restaurantId);
-    if (!restaurant || restaurant.hasAddress()) return res.status(404).send({ error: "Restaurant not found" });
-    delivery = await createDelivery(
-      new Address().setLat(user.getMainaddress()!.getLat()).setLng(user.getMainaddress()!.getLng()),
-      userId,
-      restaurantId,
-      new Address().setLat(restaurant.getAddress()!.getLat()).setLng(restaurant.getAddress()!.getLat()),
-    );
-    if (!delivery?.getId()) throw delivery;
-  } catch (error) {
-    return res.status(500).send({ error });
-  }
-
-  const orderInput = new CreateOrderRequest()
-    .setUser(miniUser)
-    .setPaymentId(paymentId)
-    .setDeliveryId(delivery.getId())
-    .setDeliveryType(deliveryType)
-    .setBasketSnapshot(orderBasket)
-    .setRestaurantId(restaurantId);
-  try {
-    const order: Order.AsObject = await new Promise((resolve, reject) => {
-      orderService.createOrder(orderInput, (error, response) => {
-        if (error) reject(error);
-        else resolve(response.toObject());
-      });
-    });
-    await resetBasketByUser(userId.toString());
-    return res.status(200).json(order);
-  } catch (error) {
-    return res.status(500).send({ error });
-  }
 });
 
 orderRoutes.get("/api/order/by-user/:userId", async (req: Request, res: Response) => {
@@ -246,8 +153,12 @@ orderRoutes.get("/api/order/by-user/:userId", async (req: Request, res: Response
 
   const orderInput = new GetOrdersByUserRequest().setId(userId);
   orderService.getOrdersByUser(orderInput, (error, response) => {
+    const orderList = response.toObject().ordersList.map(async (order) => {
+      const payment = await getPayment(order.paymentId);
+      return { ...order, payment: payment?.toObject() };
+    });
     if (error) return res.status(500).send({ error });
-    else return res.status(201).json(response.toObject());
+    else return res.status(201).json(orderList);
   });
 });
 
@@ -269,8 +180,12 @@ orderRoutes.get(
     const orderInput = new GetOrdersByStatusRequest().setStatus(Status[status]);
 
     orderService.getOrdersByStatus(orderInput, (error, response) => {
+      const orderList = response.toObject().ordersList.map(async (order) => {
+        const payment = await getPayment(order.paymentId);
+        return { ...order, payment: payment?.toObject() };
+      });
       if (error) return res.status(500).send({ error });
-      else return res.status(200).json(response.toObject());
+      else return res.status(200).json({ orderList });
     });
   },
 );
@@ -351,9 +266,11 @@ orderRoutes.get("/api/order/by-delivery/:deliveryId", async (req: Request, res: 
 
   const { deliveryId } = req.query as { deliveryId: string };
   const orderInput = new GetOrderByDeliveryRequest().setId(deliveryId);
-  orderService.getOrderByDelivery(orderInput, (error, response) => {
+  orderService.getOrderByDelivery(orderInput, async (error, response) => {
+    const order = response.toObject();
+    const payment = await getPayment(order.paymentId);
     if (error) return res.status(500).send({ error });
-    else return res.status(200).json(response.toObject());
+    else return res.status(200).json({ ...order, payment: payment?.toObject() });
   });
 });
 
@@ -380,9 +297,11 @@ orderRoutes.get("/api/order/by-payment/:paymentId", async (req: Request, res: Re
 
   const { paymentId } = req.params;
   const orderInput = new GetOrderByPaymentRequest().setId(paymentId);
-  orderService.getOrderByPayment(orderInput, (error, response) => {
+  orderService.getOrderByPayment(orderInput, async (error, response) => {
+    const order = response.toObject();
+    const payment = await getPayment(order.paymentId);
     if (error) return res.status(500).send({ error });
-    else return res.status(200).json(response.toObject());
+    else return res.status(200).json({ ...order, payment: payment?.toObject() });
   });
 });
 
@@ -415,9 +334,11 @@ orderRoutes.put("/api/order/:id", withCheck({ role: "ADMIN" }), (req: Request, r
     .setDeliveryId(deliveryId)
     .setPaymentId(paymentId)
     .setRestaurantId(restaurantId);
-  orderService.updateOrder(orderInput, (error, response) => {
+  orderService.updateOrder(orderInput, async (error, response) => {
+    const order = response.toObject();
+    const payment = await getPayment(order.paymentId);
     if (error) return res.status(500).send({ error });
-    else return res.status(200).json(response.toObject());
+    else return res.status(200).json({ ...order, payment: payment?.toObject() });
   });
 });
 
