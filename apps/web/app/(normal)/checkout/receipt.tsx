e0a99c0576c71221f02931431c040e75d9430c41
Anatole-Godard <anatole.godard@icloud.com>
2024-02-08 19:10:43 +0100

    [FEATURE]: Fix checkout callback

--- apps/web/app/(normal)/checkout/receipt.tsx
+++ apps/web/app/(normal)/checkout/receipt.tsx
@@ -1,12 +1,15 @@
 import { fetchAPI } from "@/lib/fetchAPI";
 import { toPrice } from "@/lib/product/toPrice";
-import { Order, BasketSnapshot } from "@/types/order";
+import { BasketSnapshot, Order } from "@/types/order";
 import { Restaurant } from "@/types/restaurant";
 import { format } from "date-fns";
 import fr from "date-fns/locale/fr";
 import { useQuery } from "@tanstack/react-query";
+import { Product } from "@/types/product";
+import { useEffect, useState } from "react";
 
 export const CheckoutReceipt = (order: Order) => {
+  const [productList, setProductList] = useState<{ product: Product; quantity: number }[] | null>(null);
   const { data: restaurant } = useQuery<Restaurant>({
     queryKey: ["restaurant", order.restaurantId],
     queryFn: async () => {
@@ -15,9 +18,27 @@ export const CheckoutReceipt = (order: Order) => {
       return body;
     },
   });
-  if (!restaurant) return null;
 
-  const basket: BasketSnapshot = order.basketSnapshot.json ?? JSON.parse(order.basketSnapshot.string);
+  useEffect(() => {
+    async () => {
+      const productsList = await Promise.all(
+        basket.productsList.map(async ({ id, quantity }) => {
+          const product = await getProduct(id);
+          return { product, quantity };
+        }),
+      );
+      setProductList(productsList);
+    };
+  }, []);
+
+  const getProduct = async (id: string): Promise<Product> => {
+    const res = await fetchAPI(`/api/product/${id}`, undefined);
+    return await res.json();
+  };
+
+  if (!restaurant || !productList) return null;
+  console.log(productList);
+  const basket: BasketSnapshot = JSON.parse(order.basketSnapshot.string);
 
   return (
     <div className="flex w-96 flex-col bg-[url(/images/wrinkled-paper.jpg)] bg-center p-5 font-mono text-sm uppercase">
@@ -31,10 +52,11 @@ export const CheckoutReceipt = (order: Order) => {
       </h2>
 
       <p className="m-0">
-        COMMANDE #{order.id.slice(-6)} DE {`${order.user.first_name} ${order.user.last_name}`}
+        COMMANDE #{order.id.slice(-6)} DE {`${order.payment.user.name}`}
       </p>
-      {/* todo: change date */}
-      <p className="m-0">{format(new Date(order.created_at.toString()), "eeee d MMMM yyyy à HH:mm", { locale: fr })}</p>
+      <p className="m-0">
+        {format(new Date(order.delivery.eta.toString()), "eeee d MMMM yyyy à HH:mm", { locale: fr })}
+      </p>
       <div className="my-4 flex h-full w-full grow flex-col border-0 text-xs">
         <div className="my-0.5 inline-flex w-full gap-x-2.5 border-y border-dashed border-black ">
           <span className="w-8">QTY</span>
@@ -42,11 +64,11 @@ export const CheckoutReceipt = (order: Order) => {
           <span className="w-12 text-right">AMT</span>
         </div>
         <div className="flex grow flex-col overflow-y-auto">
-          {basket.productsList.map(({ id, quantity, price, name }) => (
-            <div key={id} className="inline-flex items-center gap-x-2.5">
+          {productList.map(({ product, quantity }) => (
+            <div key={product.id} className="inline-flex items-center gap-x-2.5">
               <span className="w-8">{quantity}</span>
-              <span className="grow">{name}</span>
-              <span className="w-12 text-right">{toPrice(price * quantity)}</span>
+              <span className="grow">{product.name}</span>
+              <span className="w-12 text-right">{toPrice(product.price * quantity)}</span>
             </div>
           ))}
         </div>
@@ -57,9 +79,7 @@ export const CheckoutReceipt = (order: Order) => {
           </div>
           <div className="inline-flex items-center justify-between gap-x-2.5 border-b border-dashed border-black">
             <span className="begin">TOTAL:</span>
-            <span className="length">
-              {toPrice(basket.productsList.reduce((acc, { quantity, price }) => acc + quantity * price, 0))}
-            </span>
+            <span className="length">{toPrice(order.payment.total)}</span>
           </div>
         </div>
       </div>
