3eba91d7b46ac2ada7c46e749559f47d2ef61847
Anatole-Godard <anatole.godard@icloud.com>
2024-04-17 19:55:24 +0200

    [FEATURE]: Add logout btn

--- apps/mobile/components/ui/(app)/drawer.tsx
+++ apps/mobile/components/ui/(app)/drawer.tsx
@@ -1,15 +1,17 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { DrawerContentComponentProps, DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
-import { useNavigation } from "expo-router";
+import { router, useNavigation } from "expo-router";
+import { useAuth } from "@/hooks/useAuth";
 
 export const DrawerContent = (props: DrawerContentComponentProps) => {
   const routeName = props.state.routeNames[props.state.index];
+  const { logout } = useAuth();
   const { navigate } = useNavigation() as {
     navigate: (href: string, params?: any) => void;
   };
 
   return (
-    <DrawerContentScrollView {...props}>
+    <DrawerContentScrollView {...props} className="h-screen">
       {/* <DrawerItemList {...props} /> */}
       <DrawerItem
         label="Accueil"
@@ -34,6 +36,17 @@ export const DrawerContent = (props: DrawerContentComponentProps) => {
         icon={({ color, size }) => <MaterialCommunityIcons name="basket" color={color} size={size} />}
         focused={routeName === "orders/index"}
         onPress={() => navigate("(app)", { screen: "orders/index" })}
+      />
+      <DrawerItem
+        label="Deconnexion"
+        activeBackgroundColor="#008D5E33"
+        activeTintColor="black"
+        icon={({ color, size }) => <MaterialCommunityIcons name="logout" color={color} size={size} />}
+        focused={routeName === "login"}
+        onPress={() => {
+          logout();
+          router.push("/(auth)/login");
+        }}
       />
     </DrawerContentScrollView>
   );
