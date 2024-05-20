7546009f965bc752e5462959eaf9eff8dd26b76c
Anatole-Godard <anatole.godard@icloud.com>
2024-05-20 12:08:52 +0200

    [FEATURE]: Add payment methods for mobile

--- services/gateway/src/controller/payment/stripe.controller.ts
+++ services/gateway/src/controller/payment/stripe.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response, Router } from "express";
-import { CreateCheckoutSessionRequest } from "@gateway/proto/payment_pb";
+import { CreateCheckoutSessionRequest, CreatePaymentIntentRequest } from "@gateway/proto/payment_pb";
 import { stripeServiceClient } from "@gateway/services/clients/payment.client";
 import { getUser, getUserIdFromToken } from "@gateway/services/user.service";
 import { basketServiceClient } from "@gateway/services/clients/basket.client";
 import { Basket, UserIdRequest } from "@gateway/proto/basket_pb";
 import { productServiceClient } from "@gateway/services/clients/product.client";
 import { Product, ProductId } from "@gateway/proto/product_pb";
+import { reduceProductFromStock } from "@gateway/lib/payment";
+import { Empty } from "google-protobuf/google/protobuf/empty_pb";
 
 export const stripeRoutes = Router();
 
@@ -38,28 +40,7 @@ stripeRoutes.post("/api/payment/stripe", async (req: Request, res: Response) => 
 
   const name = `${user.getFirstName()} ${user.getLastName()}`;
   const email = user.getEmail();
-
-  const basket: Basket.AsObject = await new Promise((resolve, reject) => {
-    basketServiceClient.getBasket(new UserIdRequest().setUserId(userId), (error, response) => {
-      if (error) reject(error);
-      else resolve(response.toObject());
-    });
-  });
-
-  const products = await Promise.all(
-    basket.productsList.map(async (product) => {
-      return (await new Promise((resolve, reject) => {
-        productServiceClient.readProduct(new ProductId().setId(product.id), (error, response) => {
-          if (error) reject(error);
-          else resolve(response.toObject());
-        });
-      })) as Product.AsObject;
-    }),
-  );
-
-  const total = products.reduce((acc, product) => {
-    return acc + product.price * (basket.productsList.find((p) => p.id === product.id)?.quantity || 1);
-  }, 0);
+  const total = await reduceProductFromStock(userId.toString());
 
   const createCheckoutSessionRequest = new CreateCheckoutSessionRequest()
     .setUserId(userId.toString())
@@ -68,6 +49,56 @@ stripeRoutes.post("/api/payment/stripe", async (req: Request, res: Response) => 
     .setTotal(total)
     .setReturnUrlBase(req.body.return_url_base);
   stripeServiceClient.createCheckoutSession(createCheckoutSessionRequest, (error, response) => {
+    if (error) return res.status(500).send({ error });
+    else return res.status(200).json(response.toObject());
+  });
+});
+
+stripeRoutes.post("/api/payment/stripe/setup-intent", async (req: Request, res: Response) => {
+  /* #swagger.parameters['authorization'] = {
+        in: 'header',
+        required: true,
+        type: 'string'
+    }
+    */
+  // Auth check and :id check ---
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: "Unauthorized" });
+  const token = authorization.split("Bearer ")[1];
+  const userId = await getUserIdFromToken(token);
+  if (!userId) return res.status(401).json({ message: "Unauthorized" });
+
+  stripeServiceClient.createSetupIntent(new Empty(), (error, response) => {
+    if (error) return res.status(500).send({ error });
+    else return res.status(200).json(response.toObject());
+  });
+});
+
+stripeRoutes.post("/api/payment/stripe/create-intent", async (req: Request, res: Response) => {
+  /* #swagger.parameters['authorization'] = {
+        in: 'header',
+        required: true,
+        type: 'string'
+    }
+    */
+  // Auth check and :id check ---
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: "Unauthorized" });
+  const token = authorization.split("Bearer ")[1];
+  const userId = await getUserIdFromToken(token);
+  if (!userId) return res.status(401).json({ message: "Unauthorized" });
+  const user = await getUser(userId);
+  // ----------------------------
+
+  if (!user?.getFirstName() || !user.getLastName() || !user.getEmail())
+    return res.status(400).send({ error: "User is missing information" });
+
+  const total = await reduceProductFromStock(userId.toString());
+  if (!total || total <= 0) return res.status(400).send({ error: "No products in basket" });
+
+  const createPaymentIntentRequest = new CreatePaymentIntentRequest().setAmount(total);
+
+  stripeServiceClient.createPaymentIntent(createPaymentIntentRequest, (error, response) => {
     if (error) return res.status(500).send({ error });
     else return res.status(200).json(response.toObject());
   });
